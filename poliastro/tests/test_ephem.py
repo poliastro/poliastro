81c954814022b5dedba6909c0b70cb5e07d8705b
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2013-07-08 13:32:37 +0200

    Added unit tests and integration tests for ephem

---
+++ poliastro/tests/test_ephem.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+import numpy as np
+from numpy import radians
+from numpy.testing import TestCase, assert_almost_equal, run_module_suite
+
+from poliastro import ephem
+from poliastro.constants import AU
+
+
+class TestEphem(TestCase):
+    def test_vallado55(self):
+        dd = datetime(1994, 5, 20, 20)
+        a, ecc, inc, omega, argp, nu = ephem.ephem(dd, ephem.JUPITER)
+        assert_almost_equal(a / AU, 5.202603, decimal=6)
+        assert_almost_equal(ecc, 0.048486, decimal=6)
+        assert_almost_equal(inc, radians(1.303270), decimal=4)
+        assert_almost_equal(omega, radians(100.454519), decimal=2)
+        assert_almost_equal(argp, radians(-86.135316), decimal=2)
+        assert_almost_equal(nu, radians(206.95453))
+
+
+class TestJd(TestCase):
+    def test_vallado55(self):
+        dd = datetime(1994, 5, 20, 20)
+        assert_almost_equal(ephem.jd(dd), 2449493.333, decimal=3)
+
+
+if __name__ == '__main__':
+    run_module_suite()
---
+++ poliastro/tests/test_rv_ephem.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+import numpy as np
+from numpy import radians
+from numpy.testing import TestCase, assert_array_almost_equal, \
+    run_module_suite
+
+from poliastro import ephem, twobody
+from poliastro.constants import AU, k_Sun
+
+
+class TestRVEphem(TestCase):
+    def test_vallado55(self):
+        # Data from HORIZONS, the xy-plane is the ecliptic
+        TU = 86400
+        dd = datetime(1994, 5, 20, 20)
+        coe = ephem.ephem(dd, ephem.JUPITER)
+        r_XYZ, v_XYZ = twobody.coe2rv(k_Sun, *coe)
+        r0_XYZ = np.array([-4.0686995, -3.5897288, 0.1059762]) * AU
+        v0_XYZ = np.array([0.0048974, -0.0053138, -0.00008791]) * AU / TU
+        assert_array_almost_equal(r_XYZ / AU, r0_XYZ / AU, decimal=2)
+        assert_array_almost_equal(v_XYZ, v0_XYZ, decimal=2)
+
+if __name__ == '__main__':
+    run_module_suite()
