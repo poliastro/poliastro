Added transformations between coe and rv forms Also: * Unicode fixes in bodies and test_bodies * Added rotation functions, wrapping astropy.coordinates.angles * Cleaned __init__.py

poliastro/tests/test_util.py
@@ -1,41 +1,33 @@
 import numpy as np
-from numpy import radians
-from numpy.testing import TestCase, assert_array_almost_equal, \
-    assert_almost_equal, run_module_suite
+from numpy.testing import assert_array_almost_equal
 
-from poliastro.util import rotate, direct_angles
+from astropy import units as u
 
-
-class TestRotate(TestCase):
-    def test_rotation_around_coordinate_axis(self):
-        vec = np.array([1, 0, 0])
-        ax = 2
-        angle = np.pi / 2
-        rot_vec = rotate(vec, ax, angle)
-        assert_array_almost_equal(rot_vec, np.array([0, 0, -1]))
-
-    def test_rotation_around_arbitrary_axis(self):
-        vec = np.array([1, 0, 0])
-        ax = np.array([0, 0, 1])
-        angle = np.pi / 2
-        rot_vec = rotate(vec, ax, angle)
-        assert_array_almost_equal(rot_vec, np.array([0, 1, 0]))
-
-    def test_vectorize_vec(self):
-        vec = np.random.rand(3, 10)
-        ax = 1
-        angle = np.random.rand()
-        rot_vec = rotate(vec, ax, angle)
-        assert rot_vec.shape == vec.shape
+from poliastro import util
 
 
-class TestDirectAngles(TestCase):
-    def test_data(self):
-        rd = radians
-        assert direct_angles(0, rd(-1)) == (0, rd(359))
-        assert direct_angles(0, rd(-361)) == (0, rd(359))
-        assert direct_angles(rd(360), rd(-361)) == (rd(360), rd(719))
+def test_rotate_unitless_vector():
+    vector = [1, 0, 0]
+    angle = 90 * u.deg
+    axis = 'z'
+    expected_vector = [0, 1, 0]
+    result = util.rotate(vector, angle, axis)
+    assert_array_almost_equal(result, expected_vector)
 
 
-if __name__ == '__main__':
-    run_module_suite()
+def test_rotate_vector_with_units():
+    vector = [1, 0, 0] * u.m
+    angle = 90 * u.deg
+    axis = 'y'
+    expected_vector = [0, 0, -1] * u.m
+    result = util.rotate(vector, angle, axis)
+    assert_array_almost_equal(result, expected_vector)
+
+
+def test_transform_unitless_vector():
+    vector = [0, 1, 0]
+    angle = 45 * u.deg
+    axis = 'z'
+    expected_vector = [np.sqrt(2) / 2, np.sqrt(2) / 2, 0]
+    result = util.transform(vector, angle, axis)
+    assert_array_almost_equal(result, expected_vector)

poliastro/twobody.py
@@ -2,11 +2,14 @@
 
 """
 
+import numpy as np
+
 from astropy import units as u
 from astropy import time
 
+from poliastro.util import transform
+
 J2000 = time.Time("J2000", scale='utc')
-ELEMENTS_UNITS = (u.m, u.dimensionless_unscaled, u.rad, u.rad, u.rad, u.rad)
 
 
 class State(object):
@@ -27,19 +30,107 @@ class State(object):
             Epoch, default to J2000
 
         """
+        # TODO: Cannot define a parabola with its semi-major axis
         self.attractor = attractor
         self.epoch = epoch
         if len(values) == 6:
             _check_elements_units(values)
-            self.elements = values
+        elif len(values) == 2:
+            _check_rv_units(values)
         else:
             raise ValueError("Incorrect number of parameters")
+        self._values = values
+
+    @classmethod
+    def coe2rv(cls, attractor, elements):
+        """Return `State` object using r and v from elements.
+
+        """
+        # TODO: Desirable?
+        #ss_coe = cls(attractor, elements)
+        #ss_coe.p, ss_coe.ecc, ...
+        a, ecc, inc, raan, argp, nu = elements
+        p = a * (1 - ecc ** 2)
+        # HACK: No easy way to build vector quantities, see
+        # http://mail.scipy.org/pipermail/astropy/2013-December/002991.html
+        r_pqw = [p.to(u.km).value * np.cos(nu) / (1 + ecc * np.cos(nu)),
+                 p.to(u.km).value * np.sin(nu) / (1 + ecc * np.cos(nu)),
+                 0] * u.km
+        sqrtkp = np.sqrt(attractor.k / p).to(u.km / u.s)
+        v_pqw = [-sqrtkp.value * np.sin(nu),
+                 sqrtkp.value * (ecc + np.cos(nu)),
+                 0] * u.km / u.s
+        r_ijk = transform(r_pqw, -argp, 'z')
+        r_ijk = transform(r_ijk, -inc, 'x')
+        r_ijk = transform(r_ijk, -raan, 'z')
+        v_ijk = transform(v_pqw, -argp, 'z')
+        v_ijk = transform(v_ijk, -inc, 'x')
+        v_ijk = transform(v_ijk, -raan, 'z')
+
+        return cls(attractor, (r_ijk, v_ijk))
+
+    @classmethod
+    def rv2coe(cls, attractor, rv):
+        """Returns `State` object using elements from r and v.
+
+        """
+        # TODO: See coe2rv
+        #import pdb; pdb.set_trace()
+        # HACK: Neither np.dot nor np.cross won't preserve Quantity,
+        # see astropy #1930
+        def dot(a, b):
+            return np.sum(a * b)
+        r, v = rv
+        k = attractor.k
+        r_mag = np.sqrt(dot(r, r))
+        v2 = dot(v, v)
+        h = np.cross(r, v) * u.km ** 2 / u.s
+        n = np.cross([0, 0, 1], h) * h.unit
+        n_mag = np.sqrt(dot(n, n))
+        e = (((v2 - k / r_mag) * r - dot(r, v) * v) / k).decompose()
+        ecc = np.sqrt(dot(e, e))
+        p = dot(h, h) / k
+        a = p / (1 - ecc ** 2)  # Might produce np.inf
+        inc = np.arccos(dot(h, [0, 0, 1]) / np.sqrt(dot(h, h))).to(u.deg)
+        raan = np.arccos(dot(n, [1, 0, 0]) / np.sqrt(dot(n, n))).to(u.deg)
+        # TODO: Use of arctan2 to automatically resolve quadrants
+        if dot(n, [0, 1, 0]) < 0:
+            raan = 360 * u.deg - raan
+        argp = np.arccos(dot(n, e) / (ecc * n_mag)).to(u.deg)
+        if dot(e, [0, 0, 1]) < 0:
+            argp = 360 * u.deg - argp
+        nu = np.arccos(dot(e, r) / (ecc * r_mag)).to(u.deg)
+        if dot(r, v) < 0:
+            nu = 360 * u.deg - nu
+        return cls(attractor, (a, ecc, inc, raan, argp, nu))
+
+    @property
+    def elements(self):
+        """Classical orbital elements.
+
+        """
+        if len(self._values) == 6:
+            return self._values
+        else:
+            return self.rv2coe(self.attractor, self.rv).elements
+
+    @property
+    def rv(self):
+        """Position and velocity vectors.
+
+        """
+        if len(self._values) == 2:
+            return self._values
+        else:
+            return self.coe2rv(self.attractor, self.elements).rv
 
 
 def _check_elements_units(elements):
     """Check if orbital elements have consistent units.
 
     """
+    ELEMENTS_UNITS = (u.m, u.dimensionless_unscaled, u.rad,
+                      u.rad, u.rad, u.rad)
     for ii, unit in enumerate(ELEMENTS_UNITS):
         try:
             if elements[ii].si.unit != unit:
@@ -48,3 +139,16 @@ def _check_elements_units(elements):
             if ii != 1:
                 raise ValueError("Elements must have units "
                                  "(use astropy.units)")
+
+
+def _check_rv_units(rv):
+    """Check if r and v vectors have consistent units.
+
+    """
+    r, v = rv
+    try:
+        if r.si.unit != u.m or v.si.unit != u.m / u.s:
+            raise u.UnitsError("Units must be consistent")
+    except AttributeError:
+        raise ValueError("r and v vectors must have units "
+                         "(use astropy.units)")

poliastro/util.py
@@ -3,26 +3,36 @@
 """
 
 import numpy as np
-from numpy import cos, sin, pi
+from astropy.coordinates import angles
 
-__all__ = ['rotate', 'transform', 'normalize', 'direct_angles']
+__all__ = ['rotate', 'transform']
 
 
-def rotate(vec, ax, angle):
+def rotate(vector, angle, axis='z', unit=None):
     """Rotates a vector around axis a right-handed positive angle.
+
+    This is just a convenience function around
+    `astropy.coordinates.angles.rotation_matrix`.
 
     Parameters
     ----------
-    vec : array
+    vector : array_like
         Dimension 3 vector.
-    ax : array, int
-        Axis of rotation. If 1, 2, 3 is given, then one of the coordinate axis
-        is chosen.
-    angle : float
-        Angle of rotation (rad).
+    angle : convertible to Angle
+        Angle of rotation.
+    axis : str or 3-sequence
+        Either 'x','y', 'z', or a (x,y,z) specifying an axis to rotate
+        about. If 'x','y', or 'z', the rotation sense is
+        counterclockwise looking down the + axis (e.g. positive
+        rotations obey left-hand-rule).
+    unit : UnitBase, optional
+        If `angle` does not have associated units, they are in this
+        unit.  If neither are provided, it is assumed to be degrees.
 
     Notes
     -----
+    This is just a convenience function around
+    `astropy.coordinates.angles.rotation_matrix`.
     This performs a so-called active or alibi transformation: rotates the
     vector while the coordinate system remains unchanged. To do the opposite
     operation (passive or alias transformation) call the function as
@@ -34,63 +44,18 @@ def rotate(vec, ax, angle):
     .. [1] http://en.wikipedia.org/wiki/Rotation_matrix#Ambiguities
 
     """
-    if type(ax) is int:
-        ax_ = np.zeros(3)
-        ax_[ax - 1] = 1
-        ax = ax_
-    rot = _rot_matrix(ax, angle)
-    return np.dot(rot, vec)
+    rot = np.asarray(angles.rotation_matrix(-angle, axis, unit))
+    return np.dot(rot, vector)
 
 
-def transform(vec, ax, angle):
+def transform(vector, angle, axis='z', unit=None):
     """Rotates a coordinate system around axis a positive right-handed angle.
 
     Notes
     -----
-    This is a convenience function, equivalent to `rotate(vec, ax, -angle)`.
+    This is a convenience function, equivalent to
+    `rotate(vec, -angle, axis, unit)`.
     Refer to the documentation of that function for further information.
 
     """
-    return rotate(vec, ax, -angle)
-
-
-def _rot_matrix(ax, ang):
-    """
-    Based on
-    http://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
-
-    """
-    rot = np.array([
-        [
-            cos(ang) + ax[0] ** 2 * (1 - cos(ang)),
-            ax[0] * ax[1] * (1 - cos(ang)) - ax[2] * sin(ang),
-            ax[0] * ax[2] * (1 - cos(ang)) + ax[1] * sin(ang)
-            ],
-        [
-            ax[1] * ax[0] * (1 - cos(ang)) + ax[2] * sin(ang),
-            cos(ang) + ax[1] ** 2 * (1 - cos(ang)),
-            ax[1] * ax[2] * (1 - cos(ang)) - ax[0] * sin(ang)
-            ],
-        [
-            ax[2] * ax[0] * (1 - cos(ang)) - ax[1] * sin(ang),
-            ax[2] * ax[1] * (1 - cos(ang)) + ax[0] * sin(ang),
-            cos(ang) + ax[2] ** 2 * (1 - cos(ang))]
-    ])
-    return rot
-
-
-def normalize(angle):
-    """Normalize angle between 0 and 2 pi radians.
-
-    """
-    twopi = 2 * np.pi
-    return angle % twopi
-
-
-def direct_angles(a_i, a_f):
-    """Return angles so a_f > a_i, with a1 unchanged.
-
-    """
-    twopi = 2 * np.pi
-    a_f = a_f - twopi * ((a_f - a_i) // twopi)
-    return a_i, a_f
+    return rotate(vector, -angle, axis, unit)