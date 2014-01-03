fecd62996b32e0d7ca2d6e22e68ea9ca529f89e0
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2014-01-03 10:52:13 +0100

    Created package twobody

--- poliastro/twobody/tests/test_core.py
+++ poliastro/twobody/tests/test_core.py
@@ -7,13 +7,13 @@ from astropy import units as u
 from astropy import time
 
 from poliastro.bodies import Sun, Earth
-from poliastro import twobody
+from poliastro.twobody import core
 
 
 def test_state_raises_valueerror_if_meaningless_state():
     _ = 1.0
     with pytest.raises(ValueError) as excinfo:
-        ss = twobody.State(Sun, (_,))
+        ss = core.State(Sun, (_,))
     assert ("ValueError: Incorrect number of parameters"
             in excinfo.exconly())
 
@@ -22,7 +22,7 @@ def test_state_has_attractor_given_in_constructor():
     _d = 1.0 * u.AU  # Unused distance
     _ = 0.5  # Unused dimensionless value
     _a = 1.0 * u.deg  # Unused angle
-    ss = twobody.State(Sun, (_d, _, _a, _a, _a, _a))
+    ss = core.State(Sun, (_d, _, _a, _a, _a, _a))
     assert ss.attractor == Sun
 
 
@@ -32,7 +32,7 @@ def test_default_time_for_new_state():
     _a = 1.0 * u.deg  # Unused angle
     _body = Sun  # Unused body
     expected_epoch = time.Time("J2000", scale='utc')
-    ss = twobody.State(_body, (_d, _, _a, _a, _a, _a))
+    ss = core.State(_body, (_d, _, _a, _a, _a, _a))
     assert ss.epoch == expected_epoch
 
 
@@ -44,7 +44,7 @@ def test_state_has_elements_given_in_constructor():
     raan = 49.562 * u.deg
     argp = 286.537 * u.deg
     nu = 23.33 * u.deg
-    ss = twobody.State(Sun, (a, ecc, inc, raan, argp, nu))
+    ss = core.State(Sun, (a, ecc, inc, raan, argp, nu))
     assert ss.elements == (a, ecc, inc, raan, argp, nu)
 
 
@@ -54,7 +54,7 @@ def test_state_raises_valueerror_if_elements_units_are_missing():
     _a = 1.0 * u.deg  # Unused angle
     wrong_angle = 1.0
     with pytest.raises(ValueError) as excinfo:
-        ss = twobody.State(Sun, (_d, _, _a, _a, _a, wrong_angle))
+        ss = core.State(Sun, (_d, _, _a, _a, _a, wrong_angle))
     assert ("Elements must have units (use astropy.units)"
             in excinfo.exconly())
 
@@ -65,7 +65,7 @@ def test_state_raises_unitserror_if_elements_units_are_wrong():
     _a = 1.0 * u.deg  # Unused angle
     wrong_angle = 1.0 * u.AU
     with pytest.raises(u.UnitsError) as excinfo:
-        ss = twobody.State(Sun, (_d, _, _a, _a, _a, wrong_angle))
+        ss = core.State(Sun, (_d, _, _a, _a, _a, wrong_angle))
     assert ("UnitsError: Units must be consistent"
             in excinfo.exconly())
 
@@ -73,7 +73,7 @@ def test_state_raises_unitserror_if_elements_units_are_wrong():
 def test_state_has_rv_given_in_constructor():
     r = [1.0, 0.0, 0.0] * u.AU
     v = [0.0, 1.0e-6, 0.0] * u.AU / u.s
-    ss = twobody.State(Sun, (r, v))
+    ss = core.State(Sun, (r, v))
     assert ss.rv == (r, v)
 
 
@@ -81,7 +81,7 @@ def test_state_raises_valueerror_if_rv_units_are_missing():
     _d = [1.0, 0.0, 0.0] * u.AU
     wrong_v = [0.0, 1.0e-6, 0.0]
     with pytest.raises(ValueError) as excinfo:
-        ss = twobody.State(Sun, (_d, wrong_v))
+        ss = core.State(Sun, (_d, wrong_v))
     assert ("ValueError: r and v vectors must have units (use astropy.units)"
             in excinfo.exconly())
 
@@ -90,7 +90,7 @@ def test_state_raises_unitserror_if_rv_units_are_wrong():
     _d = [1.0, 0.0, 0.0] * u.AU
     wrong_v = [0.0, 1.0e-6, 0.0] * u.AU
     with pytest.raises(u.UnitsError) as excinfo:
-        ss = twobody.State(Sun, (_d, wrong_v))
+        ss = core.State(Sun, (_d, wrong_v))
     assert ("UnitsError: Units must be consistent"
             in excinfo.exconly())
 
@@ -107,7 +107,7 @@ def test_convert_from_rv_to_coe():
     nu = 92.335 * u.deg
     expected_r = [6525.344, 6861.535, 6449.125] * u.km
     expected_v = [4.902276, 5.533124, -1.975709] * u.km / u.s
-    r, v = twobody.State(Earth, (a, ecc, inc, raan, argp, nu)).rv
+    r, v = core.State(Earth, (a, ecc, inc, raan, argp, nu)).rv
     assert_array_almost_equal(r, expected_r, decimal=1)
     assert_array_almost_equal(v, expected_v, decimal=3)
 
@@ -117,7 +117,7 @@ def test_convert_from_coe_to_rv():
     attractor = Earth
     r = [6524.384, 6862.875, 6448.296] * u.km
     v = [4.901327, 5.533756, -1.976341] * u.km / u.s
-    a, ecc, inc, raan, argp, nu = twobody.State(Earth, (r, v)).elements
+    a, ecc, inc, raan, argp, nu = core.State(Earth, (r, v)).elements
     p = a * (1 - ecc ** 2)
     assert_almost_equal(p, 11067.79 * u.km)
     assert_almost_equal(ecc, 0.832853, decimal=3)
