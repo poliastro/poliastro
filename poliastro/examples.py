1bf1ad8e7750f45d2dd8018eb2d5e4af217dc0ce
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2014-04-19 11:12:13 +0200

    Added example data Inspired by Plyades

---
+++ poliastro/examples.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+from astropy import time
+from astropy import units as u
+u.one = u.dimensionless_unscaled  # astropy #1980
+
+from poliastro.bodies import Earth
+from poliastro.twobody import State
+
+iss = State.from_vectors(Earth,
+                         [8.59072560e2, -4.13720368e3, 5.29556871e3] * u.km,
+                         [7.37289205, 2.08223573, 4.39999794e-1] * u.km / u.s,
+                         time.Time("2013-03-18 12:00", scale='utc'))
+
+molniya = State.from_elements(Earth,
+                              (26600 * u.km, 0.75 * u.one, 63.4 * u.deg,
+                               0 * u.deg, 270 * u.deg, 80 * u.deg))
