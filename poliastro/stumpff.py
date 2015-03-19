69c82e6bc1c9188c81470db8a7a4646c7b913ea9
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2015-03-19 10:46:51 +0100

    Added pure Python Stumpff and factorial functions

---
+++ poliastro/stumpff.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+"""Stumpff functions.
+
+"""
+import numpy as np
+import numba
+from poliastro.math import _factorial as factorial
+
+
+@numba.jit('f8(f8)', nopython=True)
+def c2(psi):
+    eps = 1.0
+    if psi > eps:
+        res = (1 - np.cos(np.sqrt(psi))) / psi
+    elif psi < -eps:
+        res = (np.cosh(np.sqrt(-psi)) - 1) / (-psi)
+    else:
+        res = 1.0 / 2.0
+        delta = (-psi) / factorial(2 + 2)
+        k = 1
+        while res + delta != res:
+            res = res + delta
+            k += 1
+            delta = (-psi) ** k / factorial(2 * k + 2)
+
+    return res
+
+
+@numba.jit('f8(f8)', nopython=True)
+def c3(psi):
+    eps = 1.0
+    if psi > eps:
+        res = (np.sqrt(psi) - np.sin(np.sqrt(psi))) / (psi * np.sqrt(psi))
+    elif psi < -eps:
+        res = (np.sinh(np.sqrt(-psi)) - np.sqrt(-psi)) / (-psi * np.sqrt(-psi))
+    else:
+        res = 1.0 / 6.0
+        delta = (-psi) / factorial(2 + 3)
+        k = 1
+        while res + delta != res:
+            res = res + delta
+            k += 1
+            delta = (-psi) ** k / factorial(2 * k + 3)
+
+    return res
