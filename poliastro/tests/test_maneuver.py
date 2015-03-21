Fixed failing tests relating to units

poliastro/tests/test_maneuver.py
@@ -39,7 +39,8 @@ def test_maneuver_total_time():
     _v = np.zeros(3) * u.km / u.s  # Unused velocity
     expected_total_time = 110.0 * u.s
     man = Maneuver((dt1, _v), (dt2, _v))
-    assert_almost_equal(man.get_total_time(), expected_total_time)
+    assert_almost_equal(man.get_total_time().to(u.s).value,
+                        expected_total_time.value)
 
 
 def test_maneuver_impulse():
@@ -57,9 +58,11 @@ def test_hohmann_maneuver():
     expected_t_trans = 5.256713 * u.h
     man = Maneuver.hohmann(ss_i, Earth.R + alt_f)
     assert_almost_equal(ss_i.apply_maneuver(man).ecc, 0)
-    assert_almost_equal(man.get_total_cost().to(u.km / u.s), expected_dv,
+    assert_almost_equal(man.get_total_cost().to(u.km / u.s).value,
+                        expected_dv.value,
                         decimal=5)
-    assert_almost_equal(man.get_total_time().to(u.h), expected_t_trans,
+    assert_almost_equal(man.get_total_time().to(u.h).value,
+                        expected_t_trans.value,
                         decimal=5)
 
 
@@ -73,7 +76,9 @@ def test_bielliptic_maneuver():
     expected_t_trans = 593.919803 * u.h
     man = Maneuver.bielliptic(ss_i, Earth.R + alt_b, Earth.R + alt_f)
     assert_almost_equal(ss_i.apply_maneuver(man).ecc, 0)
-    assert_almost_equal(man.get_total_cost().to(u.km / u.s), expected_dv,
+    assert_almost_equal(man.get_total_cost().to(u.km / u.s).value,
+                        expected_dv.value,
                         decimal=5)
-    assert_almost_equal(man.get_total_time().to(u.h), expected_t_trans,
-                        decimal=3)
+    assert_almost_equal(man.get_total_time().to(u.h).value,
+                        expected_t_trans.value,
+                        decimal=2)