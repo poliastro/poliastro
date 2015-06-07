44102bd445126ee22e14dd90e4ddbf35d078b623
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2015-06-07 14:28:30 +0200

    Restored angle conversion from 0.1.x Closes #20.

---
+++ poliastro/tests/test_angles.py
@@ -0,0 +1,78 @@
+# coding: utf-8
+import pytest
+
+import numpy as np
+from numpy.testing import assert_array_almost_equal, assert_almost_equal
+
+from poliastro.twobody import angles
+
+
+def test_true_to_eccentric():
+    # Data from NASA-TR-R-158
+    data = [
+        # ecc,E (deg), nu(deg)
+        (0.0, 0.0, 0.0),
+        (0.05, 10.52321, 11.05994),
+        (0.10, 54.67466, 59.49810),
+        (0.35, 142.27123, 153.32411),
+        (0.61, 161.87359, 171.02189)
+    ]
+    for row in data:
+        ecc, expected_E, nu = row
+
+        E = angles.nu_to_E(np.deg2rad(nu), ecc)
+
+        assert_almost_equal(np.rad2deg(E), expected_E, decimal=4)
+
+
+def test_mean_to_true():
+    # Data from Schlesinger & Udick, 1912
+    data = [
+        # ecc, M (deg), nu (deg)
+        (0.0, 0.0, 0.0),
+        (0.05, 10.0, 11.06),
+        (0.06, 30.0, 33.67),
+        (0.04, 120.0, 123.87),
+        (0.14, 65.0, 80.50),
+        (0.19, 21.0, 30.94),
+        (0.35, 65.0, 105.71),
+        (0.48, 180.0, 180.0),
+        (0.75, 125.0, 167.57)
+    ]
+    for row in data:
+        ecc, M, expected_nu = row
+
+        nu = angles.M_to_nu(np.deg2rad(M), ecc)
+
+        assert_almost_equal(np.rad2deg(nu), expected_nu, decimal=2)
+
+
+def test_true_to_mean():
+    # Data from Schlesinger & Udick, 1912
+    data = [
+        # ecc, M (deg), nu (deg)
+        (0.0, 0.0, 0.0),
+        (0.05, 10.0, 11.06),
+        (0.06, 30.0, 33.67),
+        (0.04, 120.0, 123.87),
+        (0.14, 65.0, 80.50),
+        (0.19, 21.0, 30.94),
+        (0.35, 65.0, 105.71),
+        (0.48, 180.0, 180.0),
+        (0.75, 125.0, 167.57)
+    ]
+    for row in data:
+        ecc, expected_M, nu = row
+
+        M = angles.nu_to_M(np.deg2rad(nu), ecc)
+
+        assert_almost_equal(M, np.deg2rad(expected_M), decimal=3)
+
+
+def test_flight_path_angle():
+    # Data from Curtis, example 2.5
+    nu = 109.5  # deg
+    ecc = 0.6
+    expected_gamma = 35.26  # deg
+    gamma = angles.fp_angle(np.deg2rad(nu), ecc)
+    assert_almost_equal(gamma, np.deg2rad(expected_gamma), decimal=3)
