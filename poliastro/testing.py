ae604343b3c8359c01ee21f95888a6a6b71dfe86
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2015-06-29 17:10:16 +0200

    Switched to new method to measure coverage Uses the variable NUMBA_DISABLE_JIT introduced in numba 0.19. Fixes #39.

--- poliastro/testing.py
+++ poliastro/testing.py
@@ -6,14 +6,5 @@ import os.path
 import pytest
 
 
-def test(coverage=False):
-    args = [os.path.dirname(os.path.abspath(__file__))]
-    if coverage:
-        # Monkey patch jit to prevent numba.jit from shadowing coverage
-        # figures (slow)
-        import poliastro.jit
-        poliastro.jit.jit = poliastro.jit.ijit
-
-        args += ["--cov", "poliastro"]
-
-    pytest.main(args)
+def test():
+    pytest.main(os.path.dirname(os.path.abspath(__file__)))
