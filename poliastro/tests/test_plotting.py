87859bd07abff640280b72cf84a0caca1948e9c4
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2015-07-12 15:03:54 +0200

    Added tests for plotting module

--- poliastro/tests/test_plotting.py
+++ poliastro/tests/test_plotting.py
@@ -4,7 +4,10 @@ import pytest
 import astropy.units as u
 
 import matplotlib
+import matplotlib.pyplot as plt
 matplotlib.use("Agg", warn=False)  # use Agg backend for these tests
+
+from poliastro.examples import iss
 
 from poliastro.plotting import OrbitPlotter
 
@@ -23,3 +26,27 @@ def test_set_frame_raises_error_if_frame_exists():
     op.set_frame(p, q, w)
     with pytest.raises(NotImplementedError):
         op.set_frame(p, q, w)
+
+
+def test_axes_labels_and_title():
+    ax = plt.gca()
+    op = OrbitPlotter(ax)
+    ss = iss
+    op.plot(ss)
+
+    assert ax.get_xlabel() == "$x$ (km)"
+    assert ax.get_ylabel() == "$y$ (km)"
+    assert ax.get_title() == str(ss.epoch.iso)
+
+
+def test_number_of_lines_for_osculating_orbit():
+    _, (ax1, ax2) = plt.subplots(ncols=2)
+    op1 = OrbitPlotter(ax1)
+    op2 = OrbitPlotter(ax2)
+    ss = iss
+
+    l1 = op1.plot(ss, osculating=False)
+    l2 = op2.plot(ss, osculating=True)
+
+    assert len(l1) == 1
+    assert len(l2) == 2
