ad1618e572c2345fb2f5304714fdff3ce76070d2
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2015-12-13 11:37:56 +0100

    Simplified setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # coding: utf-8
 
 # http://stackoverflow.com/a/10975371/554319
@@ -5,49 +6,51 @@ import io
 from setuptools import setup, find_packages
 
 
-if __name__ == '__main__':
-    setup(name="poliastro",
-          version='0.4.0.dev0',
-          description="poliastro - Python package for Orbital Mechanics",
-          author="Juan Luis Cano",
-          author_email="juanlu001@gmail.com",
-          url="http://poliastro.github.io/",
-          download_url="https://github.com/poliastro/poliastro",
-          license="MIT",
-          keywords=[
-              "aero", "aerospace", "engineering",
-              "astrodynamics", "orbits", "kepler", "orbital mechanics"
-          ],
-          requires=["numpy",
-                    "numba",
-                    "astropy",
-                    "matplotlib",
-                    "jplephem",
-                    "scipy",
-                    "pytest"],
-          packages=find_packages('src'),
-          package_dir={'': 'src'},
-          entry_points={
-              'console_scripts': [
-                  'poliastro = poliastro.cli:main'
-              ]
-          },
-          classifiers=[
-              "Development Status :: 3 - Alpha",
-              "Intended Audience :: Education",
-              "Intended Audience :: Science/Research",
-              "License :: OSI Approved :: MIT License",
-              "Operating System :: OS Independent",
-              "Programming Language :: Python",
-              "Programming Language :: Python :: 2",
-              "Programming Language :: Python :: 2.7",
-              "Programming Language :: Python :: 3",
-              "Programming Language :: Python :: 3.3",
-              "Programming Language :: Python :: 3.4",
-              "Programming Language :: Python :: Implementation :: CPython",
-              "Topic :: Scientific/Engineering",
-              "Topic :: Scientific/Engineering :: Physics",
-              "Topic :: Scientific/Engineering :: Astronomy",
-          ],
-          long_description=io.open('README', encoding='utf-8').read(),
-          package_data={"poliastro": ['tests/*.py']})
+# http://blog.ionelmc.ro/2014/05/25/python-packaging/
+setup(
+    name="poliastro",
+    version='0.4.0.dev0',
+    description="poliastro - Python package for Orbital Mechanics",
+    author="Juan Luis Cano",
+    author_email="juanlu001@gmail.com",
+    url="http://poliastro.github.io/",
+    download_url="https://github.com/poliastro/poliastro",
+    license="MIT",
+    keywords=[
+      "aero", "aerospace", "engineering",
+      "astrodynamics", "orbits", "kepler", "orbital mechanics"
+    ],
+    requires=["numpy",
+            "numba",
+            "astropy",
+            "matplotlib",
+            "jplephem",
+            "scipy",
+            "pytest"],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    entry_points={
+      'console_scripts': [
+          'poliastro = poliastro.cli:main'
+      ]
+    },
+    classifiers=[
+      "Development Status :: 3 - Alpha",
+      "Intended Audience :: Education",
+      "Intended Audience :: Science/Research",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 2",
+      "Programming Language :: Python :: 2.7",
+      "Programming Language :: Python :: 3",
+      "Programming Language :: Python :: 3.3",
+      "Programming Language :: Python :: 3.4",
+      "Programming Language :: Python :: Implementation :: CPython",
+      "Topic :: Scientific/Engineering",
+      "Topic :: Scientific/Engineering :: Physics",
+      "Topic :: Scientific/Engineering :: Astronomy",
+    ],
+    long_description=io.open('README', encoding='utf-8').read(),
+    package_data={"poliastro": ['tests/*.py']}
+)
