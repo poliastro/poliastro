e17585f3889f056be53f3d0cea3a313cae9ceca8
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2016-03-07 21:06:17 +0100

    Parametrize benchmarks <3

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,7 +1,8 @@
+import pytest
+
 import astropy.units as u
 
 from poliastro.bodies import Earth
-
 from poliastro.iod import vallado, izzo
 
 
@@ -9,27 +10,21 @@ def lambert_solution(lambert, *args, **kwargs):
     return next(lambert(*args, **kwargs))
 
 
-def test_lambert_vallado_single_rev(benchmark):
+@pytest.mark.parametrize("lambert", [vallado.lambert, izzo.lambert])
+def test_lambert_single_rev(benchmark, lambert):
     k = Earth.k
     r0 = [15945.34, 0.0, 0.0] * u.km
     r = [12214.83399, 10249.46731, 0.0] * u.km
     tof = 76.0 * u.min
 
-    benchmark(lambert_solution, vallado.lambert, k, r0, r, tof)
+    benchmark(lambert_solution, lambert, k, r0, r, tof)
 
 
-def test_lambert_izzo_single_rev(benchmark):
-    k = Earth.k
-    r0 = [15945.34, 0.0, 0.0] * u.km
-    r = [12214.83399, 10249.46731, 0.0] * u.km
-    tof = 76.0 * u.min
-
-    benchmark(lambert_solution, izzo.lambert, k, r0, r, tof)
-
-def test_lambert_izzo_multi_rev(benchmark):
+@pytest.mark.parametrize("lambert", [izzo.lambert])
+def test_lambert_multi_rev(benchmark, lambert):
     k = Earth.k
     r0 = [22592.145603, -1599.915239, -19783.950506] * u.km
     r = [1922.067697, 4054.157051, -8925.727465] * u.km
     tof = 10 * u.h
 
-    benchmark(lambert_solution, izzo.lambert, k, r0, r, tof, M=1)
+    benchmark(lambert_solution, lambert, k, r0, r, tof, M=1)
