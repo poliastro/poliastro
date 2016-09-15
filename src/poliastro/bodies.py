ea93a18aa6a399f5d8df943530aadbc7e1fd47f6
Juan Luis Cano <juanlu001@gmail.com>
2016-09-15 12:38:05 +0200

    WIP: Make Body constructor unsafe
    One untouched test still failing

--- src/poliastro/bodies.py
+++ src/poliastro/bodies.py
@@ -17,26 +17,30 @@ class Body(object):
     """Class to represent a body of the Solar System.
 
     """
-    @u.quantity_input(k=u.km ** 3 / u.s ** 2, R=u.km)
     def __init__(self, k, name=None, symbol=None, R=0 * u.km):
         """Constructor.
 
         Parameters
         ----------
         k : Quantity
-            Standard gravitational parameter
-        name : str, optional
-            Name of the body, default to None.
-        symbol : str, optional
-            Symbol for the body, default to None.
-        R : Quantity, optional
-            Radius of the body, default to 0 km.
+            Standard gravitational parameter.
+        name : str
+            Name of the body.
+        symbol : str
+            Symbol for the body.
+        R : Quantity
+            Radius of the body.
 
         """
         self.k = k
         self.name = name
         self.symbol = symbol
         self.R = R
+
+    @classmethod
+    @u.quantity_input(k=u.km ** 3 / u.s ** 2, R=u.km)
+    def from_parameters(cls, k, name, symbol, R):
+        return cls(k, name, symbol, R)
 
     def __str__(self):
         return u"{0} ({1})".format(self.name, self.symbol)
@@ -50,7 +54,7 @@ class Body(object):
         return self.__str__()
 
 
-Sun = Body(k=132712440018 * u.km ** 3 / u.s ** 2,
-           name="Sun", symbol=u"\u2609")
-Earth = Body(k=398600 * u.km ** 3 / u.s ** 2,
+Sun = Body.from_parameters(k=132712440018 * u.km ** 3 / u.s ** 2,
+           name="Sun", symbol=u"\u2609", R=695700 * u.km)
+Earth = Body.from_parameters(k=398600 * u.km ** 3 / u.s ** 2,
              name="Earth", symbol=u"\u2641", R=R_earth.to(u.km))
