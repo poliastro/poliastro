325ef457ee111f1d186f4eb6dad71285a773563a
Antonio Hidalgo <antoniohidalgo.inves@gmail.com>
2017-08-15 23:22:02 +0200

    Fixed pytest_args

--- setup.py
+++ setup.py
@@ -19,13 +19,15 @@ with open(os.path.join("src", "poliastro", "__init__.py")) as fp:
 class PyTest(test):
     user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
 
-    def __init__(self, dist):
-        test.__init__(self, dist)
-        self.pytest_args = []
-    def run_tests(self):
-        import pytest
-        sys.exit(pytest.main(self.pytest_args))
+    def initialize_options(self):
+        test.initialize_options(self)
+        self.pytest_args = ''
 
+    def run_tests(self):
+        import shlex
+        import pytest
+
+        sys.exit(pytest.main(shlex.split(self.pytest_args)))
 
 # http://blog.ionelmc.ro/2014/05/25/python-packaging/
 setup(
