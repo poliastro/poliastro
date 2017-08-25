42d9e6eebb6b2dbbe20c45eaea1a10164e689f26
Antonio Hidalgo <antoniohidalgo.inves@gmail.com>
2017-08-25 15:33:31 +0200

    Added rotational elements to body module

--- src/poliastro/bodies.py
+++ src/poliastro/bodies.py
@@ -19,8 +19,23 @@ and a way to define new bodies (:py:class:`~Body` class).
 
 Data references can be found in :py:mod:`~poliastro.constants`
 """
+import math
+from functools import wraps
+
 from astropy import units as u
 from poliastro import constants
+
+
+def d_and_t_from_epoch(func):
+
+    @wraps(func)
+    def wrapper(epoch):
+        T = (epoch.tdb - constants.J2000).to('day').value / 36525
+        d = (epoch.tdb - constants.J2000).to('day').value
+
+        return func(T, d)
+
+    return wrapper
 
 
 class _Body(object):
@@ -29,6 +44,25 @@ class _Body(object):
 
     def __repr__(self):
         return self.__str__()
+
+    @staticmethod
+    def rot_elements_at_epoch(epoch):
+        """Provides rotational elements at epoch.
+
+        Provides north pole of body and angle to prime meridian
+
+        Parameters
+        ----------
+        epoch : ~astropy.time.Time, optional
+            Epoch, default to J2000.
+
+        Returns
+        -------
+        ra, dec, W: tuple (~astropy.units.Quantity)
+            Right ascension and declination of north pole, and angle of the prime meridian.
+
+        """
+        raise NotImplementedError('Function only defined for some Solar System bodies')
 
 
 class Body(_Body):
@@ -81,6 +115,15 @@ class _Mercury(_Body):
     symbol = u"\u263F"
     R = constants.R_mercury
 
+    @staticmethod
+    @d_and_t_from_epoch
+    def rot_elements_at_epoch(T, d):
+        ra = (281.01 - 0.033 * T) * u.deg
+        dec = (61.45 - 0.005 * T) * u.deg
+        W = (329.548 + 6.1385025 * d) * u.deg
+
+        return ra, dec, W
+
 
 class _Venus(_Body):
     parent = Sun
@@ -88,6 +131,16 @@ class _Venus(_Body):
     name = "Venus"
     symbol = u"\u2640"
     R = constants.R_venus
+
+    @staticmethod
+    @d_and_t_from_epoch
+    def rot_elements_at_epoch(T, d):
+
+        ra = (281.01 - 0.033 * T) * u.deg
+        dec = (61.45 - 0.005 * T) * u.deg
+        W = (329.548 + 6.1385025 * d) * u.deg
+
+        return ra, dec, W
 
 
 class _Earth(_Body):
@@ -97,6 +150,16 @@ class _Earth(_Body):
     symbol = u"\u2641"
     R = constants.R_earth
 
+    @staticmethod
+    @d_and_t_from_epoch
+    def rot_elements_at_epoch(T, d):
+
+        ra = (0.00 - 0.641 * T) * u.deg
+        dec = (90.00 - 0.557 * T) * u.deg
+        W = (190.147 + 360.9856235 * d) * u.deg
+
+        return ra, dec, W
+
 
 class _Mars(_Body):
     parent = Sun
@@ -104,6 +167,16 @@ class _Mars(_Body):
     name = "Mars"
     symbol = u"\u2642"
     R = constants.R_mars
+
+    @staticmethod
+    @d_and_t_from_epoch
+    def rot_elements_at_epoch(T, d):
+
+        ra = (317.68143 - 0.1061 * T) * u.deg
+        dec = (52.88650 - 0.0609 * T) * u.deg
+        W = (176.630 + 350.89198226 * d) * u.deg
+
+        return ra, dec, W
 
 
 class _Jupiter(_Body):
@@ -113,6 +186,24 @@ class _Jupiter(_Body):
     symbol = u"\u2643"
     R = constants.R_jupiter
 
+    @staticmethod
+    @d_and_t_from_epoch
+    def rot_elements_at_epoch(T, d):
+
+        Ja = (99.360714 + 4850.4046 * T) * u.deg
+        Jb = (175.895369 + 1191.9605 * T) * u.deg
+        Jc = (300.323162 + 262.5475 * T) * u.deg
+        Jd = (114.012305 + 6070.2476 * T) * u.deg
+        Je = (49.511251 + 64.3000 * T) * u.deg
+
+        ra = (268.056595 - 0.006499 * T + 0.000117 * math.sin(Ja) + 0.000938 * math.sin(Jb) +
+              0.001432 * math.sin(Jc) + 0.000030 * math.sin(Jd) + 0.002150 * math.sin(Je)) * u.deg
+        dec = (64.495303 + 0.002413 * T + 0.000050 * math.cos(Ja) + 0.000404 * math.cos(Jb) +
+               0.000617 * math.cos(Jc) - 0.000013 * math.cos(Jd) + 0.000926 * math.cos(Je)) * u.deg
+        W = (284.95 + 870.5366420 * d) * u.deg
+
+        return ra, dec, W
+
 
 class _Saturn(_Body):
     parent = Sun
@@ -120,6 +211,16 @@ class _Saturn(_Body):
     name = "Saturn"
     symbol = u"\u2644"
     R = constants.R_saturn
+
+    @staticmethod
+    @d_and_t_from_epoch
+    def rot_elements_at_epoch(T, d):
+
+        ra = (40.589 - 0.036 * T) * u.deg
+        dec = (83.537 - 0.004 * T) * u.deg
+        W = (38.90 + 810.7939024 * d) * u.deg
+
+        return ra, dec, W
 
 
 class _Uranus(_Body):
@@ -129,6 +230,15 @@ class _Uranus(_Body):
     symbol = u"\u26E2"
     R = constants.R_uranus
 
+    @staticmethod
+    @d_and_t_from_epoch
+    def rot_elements_at_epoch(T, d):
+        ra = 257.311 * u.deg
+        dec = -15.175 * u.deg
+        W = (203.81 - 501.1600928 * d) * u.deg
+
+        return ra, dec, W
+
 
 class _Neptune(_Body):
     parent = Sun
@@ -137,6 +247,15 @@ class _Neptune(_Body):
     symbol = u"\u2646"
     R = constants.R_neptune
 
+    @staticmethod
+    @d_and_t_from_epoch
+    def rot_elements_at_epoch(T, d):
+        ra = (299.36 + 0.70 * math.sin(N)) * u.deg
+        dec = (43.46 - 0.51 * math.cos(N)) * u.deg
+        W = (253.18 + 536.3128492 * d - 0.48 * math.sin(N)) * u.deg
+
+        return ra, dec, W
+
 
 class _Pluto(_Body):
     parent = Sun
@@ -144,6 +263,16 @@ class _Pluto(_Body):
     name = "Pluto"
     symbol = u"\u2647"
     R = constants.R_pluto
+
+    @staticmethod
+    @d_and_t_from_epoch
+    def rot_elements_at_epoch(T, d):
+        ra = 312.993 * u.deg
+        dec = 6.163 * u.deg
+        W = (190.147 + 360.9856235 * d) * u.deg
+
+        return ra, dec, W
+
 
 Mercury = _Mercury()
 Venus = _Venus()
--- src/poliastro/coordinates.py
+++ src/poliastro/coordinates.py
@@ -14,281 +14,6 @@ from poliastro.util import transform
 from poliastro.constants import J2000
 
 
-def mercury_rot_elements(epoch=J2000):
-    """Provides Mercury rotational elements given an epoch.
-
-    Provides north pole of body and angle to prime meridian
-
-    Parameters
-    ----------
-    epoch : ~astropy.time.Time, optional
-        Epoch, default to J2000.
-
-    Returns
-    -------
-    ra, dec, W: tuple (~astropy.units.Quantity)
-        Right ascension and declination of north pole, and angle of the prime meridian.
-
-    """
-
-    T = (epoch.tdb - J2000).to('day').value / 36525
-    d = (epoch.tdb - J2000).to('day').value
-
-    ra = (281.01 - 0.033 * T) * u.deg
-    dec = (61.45 - 0.005 * T) * u.deg
-    W = (329.548 + 6.1385025 * d) * u.deg
-
-    return ra, dec, W
-
-
-def venus_rot_elements(epoch=J2000):
-    """Provides Venus rotational elements given an epoch.
-
-    Provides north pole of body and angle to prime meridian
-
-    Parameters
-    ----------
-    epoch : ~astropy.time.Time, optional
-        Epoch, default to J2000.
-
-    Returns
-    -------
-    ra, dec, W: tuple (~astropy.units.Quantity)
-        Right ascension and declination of north pole, and angle of the prime meridian.
-
-    """
-
-    d = (epoch.tdb - J2000).to('day').value
-
-    ra = 272.76 * u.deg
-    dec = 67.16 * u.deg
-    W = (160.20 - 1.4813688 * d) * u.deg
-
-    return ra, dec, W
-
-
-def earth_rot_elements(epoch=J2000):
-    """Provides Earth rotational elements given an epoch.
-
-    Provides north pole of body and angle to prime meridian
-
-    Parameters
-    ----------
-    epoch : ~astropy.time.Time, optional
-        Epoch, default to J2000.
-
-    Returns
-    -------
-    ra, dec, W: tuple (~astropy.units.Quantity)
-        Right ascension and declination of north pole, and angle of the prime meridian.
-
-    """
-    T = (epoch.tdb - J2000).to('day').value / 36525
-    d = (epoch.tdb - J2000).to('day').value
-
-    ra = (0.00 - 0.641 * T) * u.deg
-    dec = (90.00 - 0.557 * T) * u.deg
-    W = (190.147 + 360.9856235 * d) * u.deg
-
-    return ra, dec, W
-
-
-def mars_rot_elements(epoch=J2000):
-    """Provides Mars rotational elements given an epoch.
-
-    Provides north pole of body and angle to prime meridian
-
-    Parameters
-    ----------
-    epoch : ~astropy.time.Time, optional
-        Epoch, default to J2000.
-
-    Returns
-    -------
-    ra, dec, W: tuple (~astropy.units.Quantity)
-        Right ascension and declination of north pole, and angle of the prime meridian.
-
-    """
-    T = (epoch.tdb - J2000).to('day').value / 36525
-    d = (epoch.tdb - J2000).to('day').value
-
-    ra = (317.68143 - 0.1061 * T) * u.deg
-    dec = (52.88650 - 0.0609 * T) * u.deg
-    W = (176.630 + 350.89198226 * d) * u.deg
-
-    return ra, dec, W
-
-
-def jupiter_rot_elements(epoch=J2000):
-    """Provides Jupiter rotational elements given an epoch.
-
-    Provides north pole of body and angle to prime meridian
-
-    Parameters
-    ----------
-    epoch : ~astropy.time.Time, optional
-        Epoch, default to J2000.
-
-    Returns
-    -------
-    ra, dec, W: tuple (~astropy.units.Quantity)
-        Right ascension and declination of north pole, and angle of the prime meridian.
-
-    """
-    T = (epoch.tdb - J2000).to('day').value / 36525
-    d = (epoch.tdb - J2000).to('day').value
-
-    Ja = (99.360714 + 4850.4046 * T) * u.deg
-    Jb = (175.895369 + 1191.9605 * T) * u.deg
-    Jc = (300.323162 + 262.5475 * T) * u.deg
-    Jd = (114.012305 + 6070.2476 * T) * u.deg
-    Je = (49.511251 + 64.3000 * T) * u.deg
-
-    ra = (268.056595 - 0.006499 * T + 0.000117 * math.sin(Ja) + 0.000938 * math.sin(Jb) +
-          0.001432 * math.sin(Jc) + 0.000030 * math.sin(Jd) + 0.002150 * math.sin(Je)) * u.deg
-    dec = (64.495303 + 0.002413 * T + 0.000050 * math.cos(Ja) + 0.000404 * math.cos(Jb) +
-           0.000617 * math.cos(Jc) - 0.000013 * math.cos(Jd) + 0.000926 * math.cos(Je)) * u.deg
-    W = (284.95 + 870.5366420 * d) * u.deg
-
-    return ra, dec, W
-
-
-def saturn_rot_elements(epoch=J2000):
-    """Provides Saturn rotational elements given an epoch.
-
-    Provides north pole of body and angle to prime meridian
-
-    Parameters
-    ----------
-    epoch : ~astropy.time.Time, optional
-        Epoch, default to J2000.
-
-    Returns
-    -------
-    ra, dec, W: tuple (~astropy.units.Quantity)
-        Right ascension and declination of north pole, and angle of the prime meridian.
-
-    """
-    T = (epoch.tdb - J2000).to('day').value / 36525
-    d = (epoch.tdb - J2000).to('day').value
-
-    ra = (40.589 - 0.036 * T) * u.deg
-    dec = (83.537 - 0.004 * T) * u.deg
-    W = (38.90 + 810.7939024 * d) * u.deg
-
-    return ra, dec, W
-
-
-def uranus_rot_elements(epoch=J2000):
-    """Provides Uranus rotational elements given an epoch.
-
-    Provides north pole of body and angle to prime meridian
-
-    Parameters
-    ----------
-    epoch : ~astropy.time.Time, optional
-        Epoch, default to J2000.
-
-    Returns
-    -------
-    ra, dec, W: tuple (~astropy.units.Quantity)
-        Right ascension and declination of north pole, and angle of the prime meridian.
-
-    """
-    d = (epoch.tdb - J2000).to('day').value
-
-    ra = 257.311 * u.deg
-    dec = -15.175 * u.deg
-    W = (203.81 - 501.1600928 * d) * u.deg
-
-    return ra, dec, W
-
-
-def neptune_rot_elements(epoch=J2000):
-    """Provides Uranus rotational elements given an epoch.
-
-    Provides north pole of body and angle to prime meridian
-
-    Parameters
-    ----------
-    epoch : ~astropy.time.Time, optional
-        Epoch, default to J2000.
-
-    Returns
-    -------
-    ra, dec, W: tuple (~astropy.units.Quantity)
-        Right ascension and declination of north pole, and angle of the prime meridian.
-
-    """
-    T = (epoch.tdb - J2000).to('day').value / 36525
-    N = 357.85 + 52.316 * T
-    d = (epoch.tdb - J2000).to('day').value
-
-    ra = (299.36 + 0.70 * math.sin(N)) * u.deg
-    dec = (43.46 - 0.51 * math.cos(N)) * u.deg
-    W = (253.18 + 536.3128492 * d - 0.48 * math.sin(N)) * u.deg
-
-    return ra, dec, W
-
-
-def pluto_rot_elements(epoch=J2000):
-    """Provides Pluto rotational elements given an epoch.
-
-    Provides north pole of body and angle to prime meridian
-
-    Parameters
-    ----------
-    epoch : ~astropy.time.Time, optional
-        Epoch, default to J2000.
-
-    Returns
-    -------
-    ra, dec, W: tuple (~astropy.units.Quantity)
-        Right ascension and declination of north pole, and angle of the prime meridian.
-
-    """
-    d = (epoch.tdb - J2000).to('day').value
-
-    ra = 312.993 * u.deg
-    dec = 6.163 * u.deg
-    W = (190.147 + 360.9856235 * d) * u.deg
-
-    return ra, dec, W
-
-
-def rot_elements_from_body(body, epoch=J2000):
-    """Provides rotational elements of a given body at epoch.
-
-    Provides north pole of body and angle to prime meridian
-
-    Parameters
-    ----------
-    body : ~poliastro.bodies.Body
-        Body.
-    epoch : ~astropy.time.Time, optional
-        Epoch, default to J2000.
-
-    Returns
-    -------
-    ra, dec, W: tuple (~astropy.units.Quantity)
-        Right ascension and declination of north pole, and angle of the prime meridian.
-
-    """
-
-    options = {'Mercury': mercury_rot_elements,
-               'Venus': venus_rot_elements,
-               'Earth': earth_rot_elements,
-               'Mars': mars_rot_elements,
-               'Jupiter': jupiter_rot_elements,
-               'Saturn': saturn_rot_elements,
-               'Uranus': uranus_rot_elements,
-               'Neptune': neptune_rot_elements,
-               'Pluto': pluto_rot_elements,
-               }
-
-    return options[body.name](epoch)
-
-
 def body_centered_to_icrs(r, v, source_body, epoch=J2000, rotate_meridian=False):
     """Converts position and velocity body-centered frame to ICRS.
 
@@ -313,7 +38,7 @@ def body_centered_to_icrs(r, v, source_body, epoch=J2000, rotate_meridian=False)
 
     """
 
-    ra, dec, W = rot_elements_from_body(source_body, epoch)
+    ra, dec, W = source_body.rot_elements_at_epoch(epoch)
     if rotate_meridian:
         r = transform(r, -W.value, 'z', W.unit)
         v = transform(v, -W.value, 'z', W.unit)
@@ -329,8 +54,7 @@ def body_centered_to_icrs(r, v, source_body, epoch=J2000, rotate_meridian=False)
     r_f = icrs_frame_pos_coord.xyz + r_trans2
     v_f = icrs_frame_vel_coord.xyz + v_trans2
 
-    # icrs_coord = coordinates.ICRS(x=r_f[0], y=r_f[1], z=r_f[2], v_x=v_f[0], v_y=v_f[1],
-    #                              v_z=v_f[2], representation='cartesian')
+
     return r_f.to(r.unit), v_f.to(v.unit)
 
 
@@ -357,7 +81,7 @@ def icrs_to_body_centered(r, v, target_body, epoch=J2000, rotate_meridian=False)
 
     """
 
-    ra, dec, W = rot_elements_from_body(target_body, epoch)
+    ra, dec, W = target_body.rot_elements_at_epoch(epoch)
 
     icrs_frame_pos_coord, icrs_frame_vel_coord = coordinates.get_body_barycentric_posvel(target_body.name, time=epoch)
 
