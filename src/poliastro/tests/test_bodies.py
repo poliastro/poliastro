0fe1223c0ac626c92a719232219eb35e7f906fcc
Zac Miller <zmiller1@gmail.com>
2017-09-04 13:53:23 -0400

    Add from_relative to Body class
    Add the from_relative method of creating a Body to allow easy creation
    of bodies for exoplanets and other bodies that are often described with
    mass and radius relative to Sun, Jupiter or Earth.  The from_relative
    method will take any object as a reference as long as it implements the
    attributes (k and R) and does not require the reference object to be a
    poliastro Body object.
    
    Updated the docstring on Body to remove reference to the Solar System
    as bodies may be exoplanets, stars or other extrasolar objects.
    
    Allowed for pass through of additional data to newly created bodies
    using kwargs.

--- src/poliastro/tests/test_bodies.py
+++ src/poliastro/tests/test_bodies.py
@@ -36,3 +36,39 @@ def test_earth_has_k_given_in_literature():
     expected_k = 3.986004418e14 * u.m ** 3 / u.s ** 2
     k = bodies.Earth.k
     assert_quantity_allclose(k.decompose([u.km, u.s]), expected_k)
+
+
+def test_body_kwargs():
+    name = "2 Pallas"
+    symbol = u"\u26b4"
+    k = 1.41e10 * u.m ** 3 / u.s ** 2
+    pallas2 = bodies.Body(None, k, name, symbol)
+    assert pallas2.kwargs == {}
+    pallas2 = bodies.Body(None, k, name, symbol, custom='data')
+    assert 'custom' in pallas2.kwargs
+
+
+def test_from_relative():
+    TRAPPIST1 = bodies.Body.from_relative(reference=bodies.Sun,
+                                          parent=None,
+                                          k=0.08,  # Relative to the Sun
+                                          name='TRAPPIST',
+                                          symbol=None,
+                                          R=0.114)  # Relative to the Sun
+
+    TRAPPIST1D = bodies.Body.from_relative(reference=bodies.Earth,
+                                           parent=TRAPPIST1,  # Orbits TRAPPIST1
+                                           k=0.5,
+                                           name='TRAPPIST1D',
+                                           symbol=None,
+                                           R=0.772)  # Relative to the Earth
+
+    # check values properly calculated
+    VALUECHECK = bodies.Body.from_relative(reference=bodies.Earth,
+                                           parent=TRAPPIST1,
+                                           k=1,
+                                           name='VALUECHECK',
+                                           symbol=None,
+                                           R=1)
+    assert bodies.Earth.k == VALUECHECK.k
+    assert bodies.Earth.R == VALUECHECK.R
