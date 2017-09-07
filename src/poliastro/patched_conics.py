20a9565cbb6a1c7eb561a7d3cd98335d9394616e
Pablo Rodríguez Robles <pablordrrbl@gmail.com>
2017-09-07 16:42:14 +0200

    Added exception to Moon and Pluto

--- src/poliastro/patched_conics.py
+++ src/poliastro/patched_conics.py
@@ -30,9 +30,17 @@ def compute_soi(body, a=None):
     # Compute semimajor axis at epoch J2000 for the body if it was not
     # introduced by the user
     if a is None:
-        ss = Orbit.from_body_ephem(body, J2000)
-        a = ss.a
+        try:
+            ss = Orbit.from_body_ephem(body, J2000)
+            a = ss.a
 
-    r_SOI = a * (body.k / body.parent.k)**(2 / 5)
+            r_SOI = a * (body.k / body.parent.k)**(2 / 5)
 
-    return r_SOI.decompose()
+            return r_SOI.decompose()
+
+        except KeyError:
+            print("To compute the semimajor axis for Moon",
+                  " and Pluto use the JPL ephemeris: ")
+            print(">>> from astropy.coordinates import solar_system_ephemeris")
+            print('>>> solar_system_ephemeris.set("jpl")')
+            pass
