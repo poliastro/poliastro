f0c8cb6fbc3495743356fbb1e37d39bdb5d3eef7
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2017-09-13 22:50:26 +0200

    Document NEOS coordinate frame, fix #222

--- src/poliastro/neos/__init__.py
+++ src/poliastro/neos/__init__.py
@@ -3,5 +3,11 @@
 Functions related to NEOs and different NASA APIs.
 All of them are coded as part of SOCIS 2017 proposal.
 
+Notes
+-----
+
+The orbits returned by the functions in this package are in the
+:py:class:`~poliastro.frames.HeliocentricEclipticJ2000` frame.
+
 """
 from poliastro.neos import neows, dastcom5
