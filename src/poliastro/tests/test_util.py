0b204ad73aad959d57b2110b09e3664f884f885b
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2017-10-12 00:58:55 +0200

    Add utility function to create time ranges

--- src/poliastro/tests/test_util.py
+++ src/poliastro/tests/test_util.py
@@ -1,3 +1,5 @@
+import pytest
+
 import numpy as np
 from numpy.testing import assert_allclose
 
@@ -43,3 +45,41 @@ def test_transform_unitless_vector():
     expected_vector = [np.sqrt(2) / 2, np.sqrt(2) / 2, 0]
     result = util.transform(vector, angle, axis)
     assert_allclose(result, expected_vector)
+
+
+def test_time_range_spacing_periods():
+    start_time = "2017-10-12 00:00:00"
+    end_time = "2017-10-12 00:04:00"
+    spacing = 1 * u.minute
+    periods = 5
+
+    expected_scale = "utc"
+    expected_duration = 4 * u.min
+
+    result_1 = util.time_range(start_time, spacing=spacing, periods=periods)
+    result_2 = util.time_range(start_time, end=end_time, periods=periods)
+
+    assert len(result_1) == len(result_2) == periods
+    assert result_1.scale == result_2.scale == expected_scale
+
+    assert_quantity_allclose((result_1[-1] - result_1[0]).to(u.s), expected_duration)
+    assert_quantity_allclose((result_2[-1] - result_2[0]).to(u.s), expected_duration)
+
+
+def test_time_range_requires_keyword_arguments():
+    with pytest.raises(TypeError) as excinfo:
+        util.time_range(0, 0)
+    assert "TypeError: time_range() takes 1 positional argument but" in excinfo.exconly()
+
+
+def test_time_range_raises_error_wrong_arguments():
+    exception_message = "ValueError: Either 'end' or 'spacing' must be specified"
+
+    with pytest.raises(ValueError) as excinfo_1:
+        util.time_range("2017-10-12 00:00")
+
+    with pytest.raises(ValueError) as excinfo_2:
+        util.time_range("2017-10-12 00:00", spacing=0, end=0, periods=0)
+
+    assert exception_message in excinfo_1.exconly()
+    assert exception_message in excinfo_2.exconly()
--- src/poliastro/util.py
+++ src/poliastro/util.py
@@ -4,6 +4,7 @@
 import numpy as np
 
 from astropy.coordinates import matrix_utilities
+from astropy.time import Time
 
 
 def circular_velocity(k, a):
@@ -71,3 +72,24 @@ def norm(vec):
 
     """
     return np.sqrt(vec.dot(vec))
+
+
+def time_range(start, *, periods=50, spacing=None, end=None):
+    """Generates range of astronomical times.
+
+    """
+    if not isinstance(start, Time):
+        start = Time(start)
+
+    if spacing is not None and end is None:
+        result = start + spacing * np.arange(0, periods)
+
+    elif end is not None and spacing is None:
+        if not isinstance(end, Time):
+            end = Time(end)
+        result = start + (end - start) * np.linspace(0, 1, periods)
+
+    else:
+        raise ValueError("Either 'end' or 'spacing' must be specified")
+
+    return result
