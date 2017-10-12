c6f7e780a1d0b2c9a701f79c2620602597708991
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2017-10-12 15:36:28 +0200

    Mark test as known failure
    While we wait for
    https://github.com/astropy/astropy/issues/6638
    to be fixed.

--- src/poliastro/tests/tests_twobody/test_sample.py
+++ src/poliastro/tests/tests_twobody/test_sample.py
@@ -8,8 +8,11 @@ from poliastro.bodies import Earth
 from poliastro.twobody import Orbit
 
 
-@pytest.mark.parametrize("time_of_flight", [6 * u.h, 2 * u.day])
-def test_sample_one_point_equals_propagation_big_deltas(time_of_flight):
+@pytest.mark.xfail
+@pytest.mark.parametrize("time_of_flight", [1 * u.min, 40 * u.min])
+def test_sample_one_point_equals_propagation_small_deltas(time_of_flight):
+    # Time arithmetic loses precision, see
+    # https://github.com/astropy/astropy/issues/6638
     # Data from Vallado, example 2.4
     r0 = [1131.340, -2282.343, 6672.423] * u.km
     v0 = [-5.64305, 4.30333, 2.42879] * u.km / u.s
@@ -24,9 +27,8 @@ def test_sample_one_point_equals_propagation_big_deltas(time_of_flight):
     assert (rr[0].get_xyz() == expected_ss.r).all()
 
 
-@pytest.mark.parametrize("time_of_flight", [1 * u.min, 40 * u.min])
-def test_sample_one_point_equals_propagation_small_deltas(time_of_flight):
-    # FIXME: Time arithmetic is wrong
+@pytest.mark.parametrize("time_of_flight", [6 * u.h, 2 * u.day])
+def test_sample_one_point_equals_propagation_big_deltas(time_of_flight):
     # Data from Vallado, example 2.4
     r0 = [1131.340, -2282.343, 6672.423] * u.km
     v0 = [-5.64305, 4.30333, 2.42879] * u.km / u.s
