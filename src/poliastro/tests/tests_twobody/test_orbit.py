d4c27ef7a51e47aa2264f61e0366bcfeaa91770c
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2018-05-11 12:03:54 +0200

    Cleanup

--- src/poliastro/tests/tests_twobody/test_orbit.py
+++ src/poliastro/tests/tests_twobody/test_orbit.py
@@ -10,7 +10,6 @@ from astropy import time
 from astropy.time import Time
 from astropy.coordinates import CartesianRepresentation
 
-from poliastro.twobody.rv import rv2coe
 from poliastro.bodies import Sun, Earth
 from poliastro.twobody import Orbit
 from poliastro.twobody.orbit import TimeScaleWarning
