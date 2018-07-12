7951e364662ae8ab0e66242e108564d9c11836c6
Nikita Astrakhantsev <nikita.astronaut@gmail.com>
2018-07-12 16:24:16 +0300

    added change_inc_ecc

--- src/poliastro/tests/tests_twobody/test_thrust.py
+++ src/poliastro/tests/tests_twobody/test_thrust.py
@@ -8,7 +8,71 @@ from astropy import units as u
 from poliastro.bodies import Earth
 from poliastro.twobody import Orbit
 from poliastro.twobody.propagation import cowell
-from poliastro.twobody.thrust import change_a_inc, change_argp
+from poliastro.twobody.thrust import change_a_inc, change_argp, change_inc_ecc
+
+
+@pytest.mark.parametrize("ecc_0,inc_f,expected_beta,expected_delta_V", [
+    [0.1, 20.0, 83.043, 1.6789],
+    [0.2, 20.0, 76.087, 1.6890],
+    [0.4, 20.0, 61.522, 1.7592],
+    [0.6, 16.0, 40.0, 1.7241],
+    [0.8, 10.0, 16.304, 1.9799]
+])
+def test_geo_cases_beta_and_delta_v(ecc_0, inc_f, expected_beta, expected_delta_V):
+    a = 42164  # km
+    ecc_f = 0.0
+    inc_0 = 0.0  # rad, baseline
+    argp = 0.0  # rad, the method is efficient for 0 and 180
+    f = 2.4e-7  # km / s2, unused
+
+    k = Earth.k.to(u.km**3 / u.s**2).value
+
+    inc_f = np.radians(inc_f)
+    expected_beta = np.radians(expected_beta)
+
+    s0 = Orbit.from_classical(
+        Earth,
+        a * u.km, ecc_0 * u.one, inc_0 * u.deg,
+        0 * u.deg, argp * u.deg, 0 * u.deg
+    )
+
+    _, delta_V, beta, _ = change_inc_ecc(s0, ecc_f, inc_f, f)
+
+    assert_allclose(delta_V, expected_delta_V, rtol=1e-2)
+    assert_allclose(beta, expected_beta, rtol=1e-2)
+
+
+@pytest.mark.parametrize("ecc_0,ecc_f", [
+    [0.4, 0.0],
+    [0.0, 0.4]
+])
+def test_geo_cases_numerical(ecc_0, ecc_f):
+    a = 42164  # km
+    inc_0 = 0.0  # rad, baseline
+    inc_f = (20.0 * u.deg).to(u.rad).value  # rad
+    argp = 0.0  # rad, the method is efficient for 0 and 180
+    f = 2.4e-7  # km / s2
+
+    k = Earth.k.to(u.km**3 / u.s**2).value
+
+    # Retrieve r and v from initial orbit
+    s0 = Orbit.from_classical(
+        Earth,
+        a * u.km, ecc_0 * u.one, inc_0 * u.deg,
+        0 * u.deg, argp * u.deg, 0 * u.deg
+    )
+    a_d, _, _, t_f = change_inc_ecc(s0, ecc_f, inc_f, f)
+
+    # Propagate orbit
+    r, v = cowell(s0, tof=t_f, ad=a_d, rtol=1e-8)
+
+    sf = Orbit.from_vectors(Earth,
+                            r * u.km,
+                            v * u.km / u.s,
+                            s0.epoch + t_f * u.s)
+
+    assert_allclose(sf.ecc.value, ecc_f, rtol=1e-2, atol=1e-2)
+    assert_allclose(sf.inc.to(u.rad).value, inc_f, rtol=1e-1)
 
 
 def test_soyuz_standard_gto_delta_v():
--- src/poliastro/twobody/thrust/__init__.py
+++ src/poliastro/twobody/thrust/__init__.py
@@ -1,2 +1,3 @@
 from .change_a_inc import change_a_inc
 from .change_argp import change_argp
+from .change_inc_ecc import change_inc_ecc
