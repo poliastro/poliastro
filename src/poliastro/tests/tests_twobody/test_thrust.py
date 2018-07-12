99f94322ad8c7d44b456a7e08e7740bfb38bc1db
Nikita Astrakhantsev <nikita.astronaut@gmail.com>
2018-07-12 16:40:16 +0300

    added ecc_quasioptimal

--- src/poliastro/tests/tests_twobody/test_thrust.py
+++ src/poliastro/tests/tests_twobody/test_thrust.py
@@ -8,7 +8,53 @@ from astropy import units as u
 from poliastro.bodies import Earth
 from poliastro.twobody import Orbit
 from poliastro.twobody.propagation import cowell
-from poliastro.twobody.thrust import change_a_inc, change_argp, change_inc_ecc
+from poliastro.twobody.thrust import change_a_inc, change_argp, change_inc_ecc, change_ecc_quasioptimal
+
+
+@pytest.mark.parametrize("ecc_0,ecc_f", [
+    [0.0, 0.1245],  # Reverse-engineered from results
+    [0.1245, 0.0]
+])
+def test_sso_disposal_time_and_delta_v(ecc_0, ecc_f):
+    a_0 = Earth.R.to(u.km).value + 900  # km
+    f = 2.4e-7  # km / s2, assumed constant
+
+    k = Earth.k.to(u.km**3 / u.s**2).value
+
+    expected_t_f = 29.697  # days, reverse-engineered
+    expected_delta_V = 0.6158  # km / s, lower than actual result
+    s0 = Orbit.from_classical(Earth, a_0 * u.km, ecc_0 * u.one,
+                              0 * u.deg, 0 * u.deg, 0 * u.deg, 0 * u.deg)
+    _, delta_V, t_f = change_ecc_quasioptimal(s0, ecc_f, f)
+
+    assert_allclose(delta_V, expected_delta_V, rtol=1e-4)
+    assert_allclose(t_f / 86400, expected_t_f, rtol=1e-4)
+
+
+@pytest.mark.parametrize("ecc_0,ecc_f", [
+    [0.0, 0.1245],  # Reverse-engineered from results
+    [0.1245, 0.0]
+])
+def test_sso_disposal_numerical(ecc_0, ecc_f):
+    a_0 = Earth.R.to(u.km).value + 900  # km
+    f = 2.4e-7  # km / s2, assumed constant
+
+    k = Earth.k.to(u.km**3 / u.s**2).value
+
+    # Retrieve r and v from initial orbit
+    s0 = Orbit.from_classical(Earth, a_0 * u.km, ecc_0 * u.one,
+                              0 * u.deg, 0 * u.deg, 0 * u.deg, 0 * u.deg)
+    a_d, _, t_f = change_ecc_quasioptimal(s0, ecc_f, f)
+
+    # Propagate orbit
+    r, v = cowell(s0, tof=t_f, ad=a_d, rtol=1e-8)
+
+    sf = Orbit.from_vectors(Earth,
+                            r * u.km,
+                            v * u.km / u.s,
+                            s0.epoch + t_f * u.s)
+
+    assert_allclose(sf.ecc.value, ecc_f, rtol=1e-4, atol=1e-4)
 
 
 @pytest.mark.parametrize("ecc_0,inc_f,expected_beta,expected_delta_V", [
