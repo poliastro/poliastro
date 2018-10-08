244f0385d727b09acf07c08fcfca6463aa2705b0
Ole Streicher <olebole@debian.org>
2018-10-08 09:50:31 +0200

    Define absolute tolerance when comparing with 0.0
    By default, assert_quantity_allclose() allows only a relative
    tolerance (1e-7), while the absolute tolerance is zero. For comparison
    with 0.0, this is however useless, and leads to error on some CPU
    types (arm64, i386).
    
    The value 1e-12 is chosen ad-hoc to let the test pass; it may need an
    adjustment.

--- src/poliastro/tests/test_coordinates.py
+++ src/poliastro/tests/test_coordinates.py
@@ -45,5 +45,5 @@ def test_inertial_body_centered_to_pqw():
 
     molniya_peri = Orbit.from_vectors(bodies.Earth, molniya_r_peri, molniya_v_peri, molniya.epoch)
 
-    assert_quantity_allclose(molniya_peri.e_vec[-2:], [0, 0])
+    assert_quantity_allclose(molniya_peri.e_vec[-2:], [0, 0], atol=1e-12)
     assert_quantity_allclose(norm(molniya_peri.e_vec), norm(molniya.e_vec))
