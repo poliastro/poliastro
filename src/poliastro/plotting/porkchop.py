e697763ddb0e642df021d778b4f75b6beaa62f3d
Jorge Martinez <ingenierodeaviones@gmail.com>
2019-01-26 21:24:04 +0100

    Corrected figure options
    Last figure code correction

--- src/poliastro/plotting/porkchop.py
+++ src/poliastro/plotting/porkchop.py
@@ -42,10 +42,14 @@ def _targetting(departure_body, target_body, t_launch, t_arrival):
         # Compute all the output variables
         dv_dpt = norm(v_dpt - vv_dpt_body.xyz)
         dv_arr = norm(v_arr - vv_arr_body.xyz)
-        c3_launch = dv_dpt.value ** 2
-        c3_arrival = dv_arr.value ** 2
+        c3_launch = dv_dpt ** 2
+        c3_arrival = dv_arr ** 2
 
-        return c3_launch, c3_arrival, tof.jd
+        return (
+            c3_launch.to(u.km ** 2 / u.s ** 2).value,
+            c3_arrival.to(u.km ** 2 / u.s ** 2).value,
+            tof.jd,
+        )
 
     except AssertionError:
         return None, None, None
@@ -59,7 +63,9 @@ targetting_vec = np.vectorize(
 )
 
 
-def porkchop(departure_body, target_body, launch_span, arrival_span):
+def porkchop(
+    departure_body, target_body, launch_span, arrival_span, ax=None, time_lines=True
+):
     """Plots porkchop between two bodies.
 
     Parameters
@@ -72,21 +78,26 @@ def porkchop(departure_body, target_body, launch_span, arrival_span):
         Time span for launch
     arrival_span: astropy.time.Time
         Time span for arrival
-
+    ax: matplotlib.axes.Axes:
+        For custom figures
+    
     Returns
     -------
-    dpt: np.array
-        Departure time span
-    arr: np.array
-        Arrival time span
-    deltav_dpt: np.ndarray
-        Departure velocity needed for each time of flight
-    deltav_arr: np.ndarray
-        Arrival velocity needed for each time of flight
-    c3_dpt: np.ndarray
+    c3_launch: np.ndarray
         Characteristic launch energy
-    c3_arr: np.ndarray
+    c3_arrrival: np.ndarray
         Characteristic arrival energy
+    tof: np.ndarray
+        Time of flight for each transfer
+
+    Example
+    -------
+    >>> from poliastro.plotting.porkchop import porkchop
+    >>> from poliastro.bodies import Earth, Mars
+    >>> from poliastro.util import time_range
+    >>> launch_span = time_range("2005-04-30", end="2005-10-07")
+    >>> arrival_span = time_range("2005-11-16", end="2006-12-21")
+    >>> c3dpt, c3arr, tof = porkchop(Earth, Mars, launch_span, arrival_span)
     """
 
     c3_launch, c3_arrival, tof = targetting_vec(
@@ -96,24 +107,23 @@ def porkchop(departure_body, target_body, launch_span, arrival_span):
         arrival_span[:, np.newaxis],
     )
 
-    """
-    Algorithm works: 'for each launch get all arrivals'.
-    Contourf works: 'for each Y -> all X'.
-    We need to transpose the arrays.
-    """
+    # Start drawing porkchop
 
-    fig, ax = plt.subplots(figsize=(15, 15))
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(15, 15))
+    else:
+        fig = ax.figure
+
     c3_levels = np.linspace(0, 45, 30)
-    t_levels = np.linspace(100, 500, 5)
 
-    c = plt.contourf(
+    c = ax.contourf(
         [D.to_datetime() for D in launch_span],
         [A.to_datetime() for A in arrival_span],
         c3_launch,
         c3_levels,
     )
 
-    l = plt.contour(
+    l = ax.contour(
         [D.to_datetime() for D in launch_span],
         [A.to_datetime() for A in arrival_span],
         c3_launch,
@@ -122,33 +132,41 @@ def porkchop(departure_body, target_body, launch_span, arrival_span):
         linestyles="solid",
     )
 
-    t = plt.contour(
-        [D.to_datetime() for D in launch_span],
-        [A.to_datetime() for A in arrival_span],
-        tof,
-        t_levels,
-        colors="red",
-        linestyles="dashed",
-        linewidths=3.5,
-    )
+    cbar = fig.colorbar(c)
+    cbar.set_label("km2 / s2")
+    ax.clabel(l, inline=1, fmt="%1.1f", colors="k", fontsize=10)
 
-    cbar = plt.colorbar(c)
-    cbar.set_label("$km^2/s^2$")
-    plt.clabel(l, inline=1, fmt="%1.1f", colors="k", fontsize=10)
-    plt.clabel(t, inline=1, fmt="%1.1f", colors="r", fontsize=14)
+    if time_lines:
 
-    plt.grid()
+        time_levels = np.linspace(100, 500, 5)
+
+        t = ax.contour(
+            [D.to_datetime() for D in launch_span],
+            [A.to_datetime() for A in arrival_span],
+            tof,
+            time_levels,
+            colors="red",
+            linestyles="dashed",
+            linewidths=3.5,
+        )
+
+        ax.clabel(t, inline=1, fmt="%1.1f", colors="r", fontsize=14)
+
+    ax.grid()
     fig.autofmt_xdate()
 
-    plt.title(
+    ax.set_title(
         "{} - {} for year {}, C3 Launch, TFL".format(
             departure_body.name, target_body.name, launch_span[0].datetime.year
         ),
         fontsize=14,
         fontweight="bold",
     )
+    ax.set_xlabel("Launch date", fontsize=10, fontweight="bold")
+    ax.set_ylabel("Arrival date", fontsize=10, fontweight="bold")
 
-    plt.xlabel("Launch date", fontsize=10, fontweight="bold")
-    plt.ylabel("Arrival date", fontsize=10, fontweight="bold")
-
-    return c3_launch, c3_arrival, tof
+    return (
+        c3_launch * u.km ** 2 / u.s ** 2,
+        c3_arrival * u.km ** 2 / u.s ** 2,
+        tof * u.d,
+    )
