40976ebfaf0f1fd5b2ae6d10227afd993de70cff
Angala <47700775+aglpy@users.noreply.github.com>
2019-02-28 12:51:42 +0100

    Update __init__.py

--- src/poliastro/twobody/thrust/__init__.py
+++ src/poliastro/twobody/thrust/__init__.py
@@ -3,4 +3,4 @@ from .change_argp import change_argp
 from .change_inc_ecc import change_inc_ecc
 from .change_ecc_quasioptimal import change_ecc_quasioptimal
 
-__all__ = ["change_a_inc", "change_argp", "change_inc_ecc", "change_ecc_quasioptimal"]
+__all__ = ["change_a_inc", "change_argp", "change_ecc_quasioptimal", "change_inc_ecc"]
