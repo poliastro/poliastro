ed740f1a8246311074d98d5ccc5e8509f701d6ba
Shreyas Bapat <bapat.shreyas@gmail.com>
2019-03-17 17:27:03 +0530

    Remove neos forever

--- src/poliastro/neos/__init__.py
+++ src/poliastro/neos/__init__.py
@@ -1,12 +1,3 @@
-"""Code related to NEOs.
-
-Functions related to NEOs and different NASA APIs.
-All of them are coded as part of SOCIS 2017 proposal.
-
-Notes
------
-
-The orbits returned by the functions in this package are in the
-:py:class:`~poliastro.frames.HeliocentricEclipticJ2000` frame.
-
+"""
+This module is deprecated.
 """
