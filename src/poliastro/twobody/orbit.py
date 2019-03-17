2b94d15aff1072b316209ed24a52e7089e1c6bc6
Himanshu-Garg <himanshu18337@iiitd.ac.in>
2019-03-17 23:18:51 +0530

    repairing #401 issue

--- src/poliastro/twobody/orbit.py
+++ src/poliastro/twobody/orbit.py
@@ -372,40 +372,45 @@ class Orbit(object):
 
         """
         # TODO: https://github.com/poliastro/poliastro/issues/445
-        if not epoch:
-            epoch = time.Time.now().tdb
-        elif epoch.scale != "tdb":
-            epoch = epoch.tdb
-            warn(
-                "Input time was converted to scale='tdb' with value "
-                "{}. Use Time(..., scale='tdb') instead.".format(epoch.tdb.value),
-                TimeScaleWarning,
-            )
+        try:    
+            if not epoch:
+                epoch = time.Time.now().tdb
+            elif epoch.scale != "tdb":
+                epoch = epoch.tdb
+                warn(
+                    "Input time was converted to scale='tdb' with value "
+                    "{}. Use Time(..., scale='tdb') instead.".format(epoch.tdb.value),
+                    TimeScaleWarning,
+                )
 
-        r, v = get_body_barycentric_posvel(body.name, epoch)
+            r, v = get_body_barycentric_posvel(body.name, epoch)
 
-        if body == Moon:
-            # TODO: The attractor is in fact the Earth-Moon Barycenter
-            icrs_cart = r.with_differentials(v.represent_as(CartesianDifferential))
-            gcrs_cart = (
-                ICRS(icrs_cart)
-                .transform_to(GCRS(obstime=epoch))
-                .represent_as(CartesianRepresentation)
-            )
-            ss = cls.from_vectors(
-                Earth,
-                gcrs_cart.xyz.to(u.km),
-                gcrs_cart.differentials["s"].d_xyz.to(u.km / u.day),
-                epoch,
-            )
+            if body == Moon:
+                # TODO: The attractor is in fact the Earth-Moon Barycenter
+                icrs_cart = r.with_differentials(v.represent_as(CartesianDifferential))
+                gcrs_cart = (
+                    ICRS(icrs_cart)
+                    .transform_to(GCRS(obstime=epoch))
+                    .represent_as(CartesianRepresentation)
+                )
+                ss = cls.from_vectors(
+                    Earth,
+                    gcrs_cart.xyz.to(u.km),
+                    gcrs_cart.differentials["s"].d_xyz.to(u.km / u.day),
+                    epoch,
+                )
 
-        else:
-            # TODO: The attractor is not really the Sun, but the Solar System
-            # Barycenter
-            ss = cls.from_vectors(Sun, r.xyz.to(u.km), v.xyz.to(u.km / u.day), epoch)
-            ss._frame = ICRS()  # Hack!
+            else:
+                # TODO: The attractor is not really the Sun, but the Solar System
+                # Barycenter
+                ss = cls.from_vectors(Sun, r.xyz.to(u.km), v.xyz.to(u.km / u.day), epoch)
+                ss._frame = ICRS()  # Hack!
 
-        return ss
+            return ss
+        except KeyError:
+            import sys
+            sys.exit("new error message here")
+
 
     @classmethod
     def from_horizons(
