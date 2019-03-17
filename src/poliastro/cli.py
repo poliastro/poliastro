0c18f604e5c33770dde90b5b2b2626ebb89d472e
Shreyas Bapat <bapat.shreyas@gmail.com>
2019-03-17 17:37:47 +0530

    Remove dastcom from cli

--- src/poliastro/cli.py
+++ src/poliastro/cli.py
@@ -4,7 +4,6 @@
 import argparse
 
 import poliastro
-from poliastro.neos.dastcom5 import download_dastcom5
 
 
 def main():
@@ -13,13 +12,7 @@ def main():
         description="Command line tools for the poliastro Python library.",
     )
     parser.add_argument("--version", action="version", version=poliastro.__version__)
-    parser.add_argument(
-        "--download-dastcom5", action="store_true", help="Downloads DASTCOM5 database"
-    )
 
     args = parser.parse_args()
 
-    if args.download_dastcom5:
-        download_dastcom5()
-    else:
-        parser.print_help()
+    parser.print_help()
