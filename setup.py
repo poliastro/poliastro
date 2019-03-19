b41c6a10f7ff340f718ef36a6e277d86a2ff5a56
Jorge <ingenierodeaviones@gmail.com>
2019-03-20 00:15:03 +0100

    Add setup.cfg true

--- setup.py
+++ setup.py
@@ -5,11 +5,4 @@ from setuptools import setup
 # https://packaging.python.org/guides/single-sourcing-package-version/
 # http://blog.ionelmc.ro/2014/05/25/python-packaging/
 
-"""
-setup(
-    setup_requires=['setup.cfg'],
-    setup_cfg=True
-)
-"""
-
-setup()
+setup(setup_cfg=True)
