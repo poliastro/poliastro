d34a5d0df67c1aa484651319b2ac6d2e35fd04c8
Meuge <euge672@gmail.com>
2019-03-20 01:40:35 -0300

    Final countdown, take 3!

--- src/poliastro/twobody/orbit.py
+++ src/poliastro/twobody/orbit.py
@@ -22,12 +22,12 @@ from poliastro.plotting.core import OrbitPlotter2D, OrbitPlotter3D
 from poliastro.twobody.angles import E_to_nu, M_to_nu, nu_to_M
 from poliastro.twobody.propagation import mean_motion, propagate
 from poliastro.util import (
+    find_closest_value,
     get_eccentricity_critical_argp,
     get_eccentricity_critical_inc,
     get_inclination_critical_argp,
     hyp_nu_limit,
     norm,
-    find_closest_value,
 )
 
 from ._states import BaseState, ClassicalState, ModifiedEquinoctialState, RVState
