3389758819d695b16260e43ec2c1425731304200
Shreyas Bapat <bapat.shreyas@gmail.com>
2019-04-05 12:32:15 +0530

    Raise notimplemented error

--- src/poliastro/neos/__init__.py
+++ src/poliastro/neos/__init__.py
@@ -1,4 +1,4 @@
-""Code related to NEOs.
+"""Code related to NEOs.
 
 Functions related to NEOs and different NASA APIs.
 All of them are coded as part of SOCIS 2017 proposal.
@@ -10,3 +10,18 @@ The orbits returned by the functions in this package are in the
 :py:class:`~poliastro.frames.HeliocentricEclipticJ2000` frame.
 
 """
+
+def neos():
+    raise NotImplementedError(
+        """This module is deprecated from poliastro and a wrapper
+        is provided in ~poliastro.twobody.orbit.Orbit for the same.
+        For using it, do:
+
+>>> from poliastro.twobody.orbit import Orbit
+>>> Orbit.from_sbdb("Neos Name")
+
+instead."""
+    )
+
+
+neows = neos()
