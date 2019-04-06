b5c2442fad462e94929846d90cf466088db46948
Shreyas Bapat <bapat.shreyas@gmail.com>
2019-04-06 14:41:21 +0530

    Remove warning

--- src/poliastro/neos/__init__.py
+++ src/poliastro/neos/__init__.py
@@ -10,19 +10,3 @@ The orbits returned by the functions in this package are in the
 :py:class:`~poliastro.frames.HeliocentricEclipticJ2000` frame.
 
 """
-
-
-def neos():
-    raise NotImplementedError(
-        """This module is deprecated from poliastro and a wrapper
-        is provided in ~poliastro.twobody.orbit.Orbit for the same.
-        For using it, do:
-
->>> from poliastro.twobody.orbit import Orbit
->>> Orbit.from_sbdb("Neos Name")
-
-instead."""
-    )
-
-
-neows = neos()
