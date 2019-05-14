ef0993be1221290c496302a2e5fb196ddc7adc44
Wil Selwood <wil.selwood@sa.catapult.org.uk>
2019-05-14 15:21:12 +0100

    reformat util

--- src/poliastro/core/util.py
+++ src/poliastro/core/util.py
@@ -173,22 +173,10 @@ def rotation_matrix(angle, axis):
     c = cos(angle)
     s = sin(angle)
     if axis == 0:
-        return np.array([
-            [1.0, 0.0, 0.0],
-            [0.0, c, -s],
-            [0.0, s, c]
-        ])
+        return np.array([[1.0, 0.0, 0.0], [0.0, c, -s], [0.0, s, c]])
     elif axis == 1:
-        return np.array([
-            [c, 0.0, s],
-            [0.0, 1.0, 0.0],
-            [s, 0.0, c]
-        ])
+        return np.array([[c, 0.0, s], [0.0, 1.0, 0.0], [s, 0.0, c]])
     elif axis == 2:
-        return np.array([
-            [c, -s, 0.0],
-            [s, c, 0.0],
-            [0.0, 0.0, 1.0]
-        ])
+        return np.array([[c, -s, 0.0], [s, c, 0.0], [0.0, 0.0, 1.0]])
     else:
         raise ValueError("Invalid axis: must be one of 'x', 'y' or 'z'")
