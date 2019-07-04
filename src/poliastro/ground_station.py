0062b43fc6a14bc27bd305c550abb4ee91f1a78b
Eleftheria Chatziargyriou <ele.hatzy@gmail.com>
2019-07-04 10:06:23 +0100

    Add coordinates in inertial frame

--- src/poliastro/ground_station.py
+++ src/poliastro/ground_station.py
@@ -1,11 +1,12 @@
 import numpy as np
+from astropy import time, units as u
 
 
 class GroundStation(object):
     """ Class representing a ground station on an arbitrary ellipsoid.
     """
 
-    def __init__(self, lon, lat, h, a, b, c = None):
+    def __init__(self, lon, lat, h, epoch, angular_velocity, a, b, c=None):
         """
 
         Parameters
@@ -18,6 +19,12 @@ class GroundStation(object):
 
         h: ~astropy.units.quantity.Quantity
             geodetic height
+
+        epoch: ~astropy.units.quantity.Quantity
+            starting epoch
+
+        angular_velocity: ~astropy.units.quantity.Quantity
+            angular velocity
 
         a: ~astropy.units.quantity.Quantity
             semi-major axis
@@ -33,13 +40,16 @@ class GroundStation(object):
         self._lon = lon
         self._lat = lat
         self._h = h
+        self._epoch = epoch
+        self._angular_velocity = angular_velocity
         self._a = a
         self._b = b
         self._c = c if c is not None else a
 
     @property
     def cartesian_cords(self):
-        """Convert to the Cartesian Coordinate system. """
+        """Convert to the Cartesian Coordinate system.
+        """
         e2 = 1 - (self._b / self._a) ** 2
         N = self._a / np.sqrt(1 - e2 * np.sin(self._lon) ** 2)
 
@@ -50,21 +60,24 @@ class GroundStation(object):
 
     @property
     def f(self):
-        """Get first flattening. """
+        """Get first flattening.
+        """
         return 1 - self._b / self._a
 
     @property
     def N(self):
-        """Normal vector of the ellipsoid at the point of the ground station. """
+        """Normal vector of the ellipsoid at the point of the ground station.
+        """
         x, y, z = self.cartesian_cords
         a, b, c = self._a.value, self._b.value, self._c.value
-        N = np.array([2 * x.value / a**2, 2 * y.value / b**2, 2 * z.value / c**2])
-        N = N / np.linalg.norm(N)
+        N = np.array([2 * x.value / a ** 2, 2 * y.value / b ** 2, 2 * z.value / c ** 2])
+        N /= np.linalg.norm(N)
         return N
 
     @property
     def tangential_vecs(self):
-        """Returns orthonormal vectors tangential to the ellipsoid at the point of the ground station. """
+        """Returns orthonormal vectors tangential to the ellipsoid at the point of the ground station.
+        """
         N = self.N
         u = np.array([1.0, 0, 0])
         u -= u.dot(N) * N
@@ -72,6 +85,41 @@ class GroundStation(object):
         v = np.cross(N, u)
         return u, v
 
+    def propagate(self, t):
+        """
+        Parameters
+        ----------
+        t: ~astropy.time.Time, ~astropy.time.TimeDelta
+            Time to propagate, either an epoch
+        """
+        if isinstance(t, time.Time):
+            delta_t = t - self._epoch
+        elif isinstance(t, time.TimeDelta):
+            delta_t = t
+        else:
+            raise ValueError(
+                "t must be either of astropy.time.Time or astropy.time.TimeDelta type"
+            )
+        angle = delta_t * self._angular_velocity
+        self._lat = (self._lat + angle) % (360 * u.deg)
+
+    def distance(self, px, py, pz):
+        """
+        Calculates the distance from point to ground station.
+
+        Parameters
+        ----------
+        px: ~astropy.units.quantity.Quantity
+            x-coordinate of the point
+        py: ~astropy.units.quantity.Quantity
+            y-coordinate of the point
+        pz: ~astropy.units.quantity.Quantity
+            z-coordinate of the point
+        """
+        c = np.array([c.value for c in self.cartesian_cords])
+        u = np.array([px.value, py.value, pz.value])
+        d = np.linalg.norm(c - u)
+        return d
 
     def is_visible(self, px, py, pz):
         """
--- src/poliastro/tests/test_ground_station.py
+++ src/poliastro/tests/test_ground_station.py
@@ -1,6 +1,7 @@
 from astropy import units as u
 from astropy.tests.helper import assert_quantity_allclose
 
+from poliastro.constants import J2000
 from poliastro.ground_station import GroundStation
 
 
@@ -14,7 +15,7 @@ def test_cartesian_coordinates():
     el_cords = [38.43 * u.deg, 41.2 * u.deg, 0 * u.m]
     ellipsoid = [6378137 * u.m, 6356752.314245 * u.m]
 
-    gs = GroundStation(*el_cords, *ellipsoid)
+    gs = GroundStation(*el_cords, J2000, 360.9856 * u.rad / u.day, *ellipsoid)
     c_cords = gs.cartesian_cords
 
     assert_quantity_allclose(c_cords, expected_cords)
@@ -24,7 +25,7 @@ def test_tangential_vectors():
     el_cords = [38.43 * u.deg, 41.2 * u.deg, 0 * u.m]
     ellipsoid = [6378137 * u.m, 6356752.314245 * u.m]
 
-    gs = GroundStation(*el_cords, *ellipsoid)
+    gs = GroundStation(*el_cords, J2000, 360.9856 * u.rad / u.day, *ellipsoid)
 
     N = gs.N
     v1, v2 = gs.tangential_vecs
@@ -37,7 +38,7 @@ def test_visible():
     el_cords = [38.43 * u.deg, 41.2 * u.deg, 0 * u.m]
     ellipsoid = [6378137 * u.m, 6356752.314245 * u.m]
 
-    gs = GroundStation(*el_cords, *ellipsoid)
+    gs = GroundStation(*el_cords, J2000, 360.9856 * u.rad / u.day, *ellipsoid)
 
     cords = gs.cartesian_cords
 
@@ -48,3 +49,21 @@ def test_visible():
 
     assert gs.is_visible(*p1) == True
     assert gs.is_visible(*p2) == False
+
+
+def test_propagate():
+    expected_cords = [
+        3707017.86447794 * u.m,
+        3359621.24213846 * u.m,
+        3942945.2857615 * u.m,
+    ]
+
+    el_cords = [38.43 * u.deg, 41.2 * u.deg, 0 * u.m]
+    ellipsoid = [6378137 * u.m, 6356752.314245 * u.m]
+
+    gs = GroundStation(*el_cords, J2000, 360.9856 * u.deg / u.day, *ellipsoid)
+    gs.propagate(J2000 + 1 * u.day)
+
+    c_cords = gs.cartesian_cords
+
+    assert_quantity_allclose(c_cords, expected_cords)
