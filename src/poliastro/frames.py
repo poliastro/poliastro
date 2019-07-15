a3cf07af20d7afce83f4fba42093e6fa9e970bfe
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2019-07-15 11:49:15 +0200

    Use new ecliptic frame from Astropy

--- src/poliastro/frames.py
+++ src/poliastro/frames.py
@@ -15,6 +15,7 @@ from astropy.coordinates import (
     CartesianDifferential,
     CartesianRepresentation,
     FunctionTransformWithFiniteDifference,
+    HeliocentricEclipticIAU76 as HeliocentricEclipticJ2000,
     TimeAttribute,
     UnitSphericalRepresentation,
     frame_transform_graph,
@@ -51,64 +52,6 @@ class Planes(Enum):
     EARTH_EQUATOR = "Earth mean Equator and Equinox of epoch (J2000.0)"
     EARTH_ECLIPTIC = "Earth mean Ecliptic and Equinox of epoch (J2000.0)"
     # BODY_EQUATOR = 'Body mean Equator and node of date'  # TODO: Implement proper conversions
-
-
-class HeliocentricEclipticJ2000(BaseEclipticFrame):
-    """
-    Heliocentric ecliptic coordinates.  These origin of the coordinates are the
-    center of the sun, with the x axis pointing in the direction of
-    the mean equinox of J2000 and the xy-plane in the plane of the
-    ecliptic of J2000 (according to the IAU 1976/1980 obliquity model).
-
-    """
-
-    obstime = TimeAttribute(default=DEFAULT_OBSTIME)
-
-
-def _ecliptic_rotation_matrix():
-    jd1, jd2 = get_jd12(J2000, J2000.scale)
-    obl = erfa.obl80(jd1, jd2) * u.radian
-    assert obl.to(u.arcsec).value == 84381.448
-    return rotation_matrix(obl, "x")
-
-
-@frame_transform_graph.transform(
-    FunctionTransformWithFiniteDifference,
-    ICRS,
-    HeliocentricEclipticJ2000,
-    finite_difference_frameattr_name="obstime",
-)
-def _icrs_to_ecliptic(from_coo, to_frame):
-    # get barycentric sun coordinate
-    bary_sun_pos = get_body_barycentric("sun", to_frame.obstime)
-
-    # offset to heliocentric
-    heliocart = from_coo.cartesian - bary_sun_pos
-
-    # now compute the matrix to precess to the right orientation
-    rmat = _ecliptic_rotation_matrix()
-
-    newrepr = heliocart.transform(rmat)
-    return to_frame.realize_frame(newrepr)
-
-
-@frame_transform_graph.transform(
-    FunctionTransformWithFiniteDifference,
-    HeliocentricEclipticJ2000,
-    ICRS,
-    finite_difference_frameattr_name="obstime",
-)
-def _ecliptic_to_icrs(from_coo, to_frame):
-    # first un-precess from ecliptic to ICRS orientation
-    rmat = _ecliptic_rotation_matrix()
-    intermed_repr = from_coo.cartesian.transform(matrix_transpose(rmat))
-
-    # now offset back to barycentric, which is the correct center for ICRS
-    # get barycentric sun coordinate
-    bary_sun_pos = get_body_barycentric("sun", from_coo.obstime)
-
-    newrepr = intermed_repr + bary_sun_pos
-    return to_frame.realize_frame(newrepr)
 
 
 # ---
