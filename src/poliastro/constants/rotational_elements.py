fd6e03cb6ebb29fc563bd273b776d525e60b612d
Eleftheria Chatziargyriou <ele.hatzy@gmail.com>
2019-07-29 13:33:18 +0100

    Add rotational periods

---
+++ src/poliastro/constants/rotational_elements.py
@@ -0,0 +1,121 @@
+from astropy.constants import Constant
+
+T_earth = Constant(
+    "T_earth",
+    "Earth rotational period",
+    0.9972698,
+    "d",
+    0,
+    "Vallado, D. and McClain, W. Fundamentals of astrodynamics and applications.",
+    system="si"
+)
+
+T_Sun = Constant(
+    "T_sun",
+    "Sun rotational period",
+    25.38,
+    "d",
+    0,
+    "Vallado, D. and McClain, W. Fundamentals of astrodynamics and applications.",
+    system="si",
+)
+
+T_Mercury = Constant(
+    "T_Mercury",
+    "Mercury rotational period",
+    58.6462,
+    "d",
+    0,
+    "Vallado, D. and McClain, W. Fundamentals of astrodynamics and applications.",
+    system="si",
+)
+
+T_Venus = Constant(
+    "T_Venus",
+    "Venus rotational period",
+    -243.01,
+    "d",
+    0,
+    "",
+    system="si",
+)
+
+T_Moon = Constant(
+    "T_Moon",
+    "Moon rotational period",
+    27.32166,
+    "d",
+    0,
+    "Vallado, D. and McClain, W. Fundamentals of astrodynamics and applications.",
+    system="si",
+)
+
+T_Mars = Constant(
+    "T_Mars",
+    "Mars rotational period",
+    1.02595675,
+    "d",
+    0,
+    "Vallado, D. and McClain, W. Fundamentals of astrodynamics and applications.",
+    system="si",
+)
+
+T_Jupiter = Constant(
+    "T_Jupiter",
+    "Jupiter rotational period",
+    0.41354,
+    "d",
+    0,
+    "Vallado, D. and McClain, W. Fundamentals of astrodynamics and applications.",
+    system="si",
+)
+
+T_Saturn = Constant(
+    "T_Saturn",
+    "Saturn rotational period",
+    0.4375,
+    "d",
+    0,
+    "Vallado, D. and McClain, W. Fundamentals of astrodynamics and applications.",
+    system="si",
+)
+
+T_Uranus = Constant(
+    "T_Uranus",
+    "Uranus rotational period",
+    -0.65,
+    "d",
+    0,
+    "Vallado, D. and McClain, W. Fundamentals of astrodynamics and applications.",
+    system="si",
+)
+
+T_Neptune = Constant(
+    "T_Neptune",
+    "Neptune rotational period",
+    0.768,
+    "d",
+    0,
+    "",
+    system="si",
+)
+
+T_Pluto = Constant(
+    "T_Pluto",
+    "Pluto rotational period",
+    -6.3867,
+    "d",
+    0,
+    "Vallado, D. and McClain, W. Fundamentals of astrodynamics and applications.",
+    system="si",
+)
+
+T_Pluto = Constant(
+    "T_Pluto",
+    "Pluto rotational period",
+    -6.38718,
+    "d",
+    0,
+    "Vallado, D. and McClain, W. Fundamentals of astrodynamics and applications.",
+    system="si",
+)
