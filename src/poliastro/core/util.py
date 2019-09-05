e2383dadfd53a0cdea7525d9c870cc974db89846
Juan Luis Cano Rodríguez <hello@juanlu.space>
2019-09-05 18:12:15 +0200

    Test using NumPy norm

--- src/poliastro/core/util.py
+++ src/poliastro/core/util.py
@@ -3,6 +3,8 @@ from numpy import cos, sin
 
 from ._jit import jit
 
+from numpy.linalg import norm
+
 
 @jit
 def circular_velocity(k, a):
@@ -86,30 +88,6 @@ def transform(vec, angle, axis):
 
     """
     return rotate(vec, -angle, axis)
-
-
-@jit
-def norm(vec):
-    r"""Returns the norm of a 3 dimension vector.
-
-    .. math::
-
-        \left \| \vec{v} \right \| = \sqrt{\sum_{i=1}^{n}v_{i}^2}
-
-    Parameters
-    ----------
-    vec: ndarray
-        Dimension 3 vector.
-
-    Examples
-    --------
-    >>> vec = np.array([1, 1, 1])
-    >>> norm(vec)
-    1.7320508075688772
-
-    """
-    vec = 1.0 * vec  # Cast to float
-    return np.sqrt(vec.dot(vec))
 
 
 @jit
