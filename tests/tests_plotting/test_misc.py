1f690d99fa1270939c93112922c636333c432a00
Juan Luis Cano Rodríguez <hello@juanlu.space>
2020-03-29 21:26:21 +0200

    Add image tests for plot_solar_system

--- tests/tests_plotting/test_misc.py
+++ tests/tests_plotting/test_misc.py
@@ -1,4 +1,6 @@
+import matplotlib.pyplot as plt
 import pytest
+from astropy.time import Time
 
 from poliastro.plotting import OrbitPlotter2D, OrbitPlotter3D
 from poliastro.plotting.misc import plot_solar_system
@@ -14,3 +16,17 @@ def test_plot_solar_system_has_expected_number_of_orbits(outer, expected):
 )
 def test_plot_solar_system_uses_expected_orbitplotter(use_3d, plotter_class):
     assert isinstance(plot_solar_system(use_3d=use_3d, interactive=True), plotter_class)
+
+
+@pytest.mark.mpl_image_compare
+def test_plot_inner_solar_system_static():
+    plot_solar_system(outer=False, epoch=Time("2020-03-29 12:00:00", scale="tdb"))
+
+    return plt.gcf()
+
+
+@pytest.mark.mpl_image_compare
+def test_plot_outer_solar_system_static():
+    plot_solar_system(outer=True, epoch=Time("2020-03-29 12:00:00", scale="tdb"))
+
+    return plt.gcf()
