209829c424a54b8b0834c2b3d90297c88590aff0
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2020-07-13 10:09:26 -0300

    Add option to skip compiling of jupyter notebooks on documentation
    Jupyter notebooks take a long time to compile. This change adds the option for the contributor to set the following environment variable to skip them while generatting docs:
    
    POLIASTRO_SKIP_NOTEBOOKS=True
    
    Discussion about it on #365

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -140,12 +140,12 @@ intersphinx_mapping = {
     'matplotlib': ('https://matplotlib.org', None)
 }
 
-"""
-if os.environ.get('READTHEDOCS') == 'True':
+if os.environ.get('POLIASTRO_SKIP_NOTEBOOKS') == 'True':
     nbsphinx_execute = 'never'
 else:
     nbsphinx_execute = 'always'
 
+"""
     # Controls when a cell will time out (defaults to 30; use -1 for no timeout):
     nbsphinx_timeout = 60
 """
