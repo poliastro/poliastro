cd6aa478dba724b3697e7f3e0ead38f23927f8fd
Jorge M.G <jorge@on-orbit.dev>
2020-08-27 19:02:31 +0200

    Add earth plotting utils and groundtrack

---
+++ src/poliastro/earth/plotting/utils.py
@@ -0,0 +1,9 @@
+""" Holds utilities related with Earth plotting """
+
+EARTH_PALETTE = {
+    "land_color": "#9fc164",
+    "ocean_color": "#b2d9ff",
+    "lake_color": "#e9eff9",
+    "dessert_color": "d8c596",
+}
+""" A color palette based on Earth colors """
