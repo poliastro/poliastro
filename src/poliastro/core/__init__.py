0a18ce99a6f6f8311b1becb01b59ac308503091a
Dhruv <dhruvsondhi05@gmail.com>
2021-02-14 11:56:30 +0530

    Added Description for core package

---
+++ src/poliastro/core/__init__.py
@@ -0,0 +1,20 @@
+"""
+This is the __init__ file for initialisation of the core module.
+This module contains the following sub-modules:
+- angles.py
+- elements.py
+- hyper.py
+- iod.py
+- perturbations.py
+- stumpff.py
+- util.py
+- Propagation
+--- __init__.py
+--- farnocchia.py
+- Thrust
+--- __init__.py
+--- change_a_inc.py
+--- change_argp.py
+--- change_ecc_quasioptimal.py
+--- change_inc_ecc.py
+"""
---
+++ src/poliastro/core/thrust/__init__.py
@@ -0,0 +1,8 @@
+"""
+This is the __init__ file for the initialisation of the Thrust sub-module
+This sub-module contains the following files:
+- change_a_inc.py
+- change_argp.py
+- change_ecc_quasioptimal.py
+- change_inc_ecc.py
+"""
