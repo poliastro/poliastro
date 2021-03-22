7e9fb7a94be49daf26ffb4bf89cb10e5a78246c1
Deepak <deepaksingh20899@gmail.com>
2021-03-22 18:10:58 +0530

    test

--- tests/tests_animation/test_anim.py
+++ tests/tests_animation/test_anim.py
@@ -1,9 +1,6 @@
 import matplotlib
-from astropy.time import Time
-from matplotlib import pyplot as plt
 
-from poliastro.examples import churi, iss, molniya
-from poliastro.frames import Planes
+from poliastro.examples import iss
 from poliastro.plotting.static import StaticOrbitPlotter
 
 
