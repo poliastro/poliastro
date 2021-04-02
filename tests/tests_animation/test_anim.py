6bc0ea5eae34e40b6ef2bef83fe457eef2036e91
Deepak <deepaksingh20899@gmail.com>
2021-04-02 17:44:09 +0530

    test modification

--- tests/tests_animation/test_anim.py
+++ tests/tests_animation/test_anim.py
@@ -1,11 +1,11 @@
 import matplotlib
 
 from poliastro.examples import iss
-from poliastro.plotting.static import StaticOrbitPlotter
+from poliastro.plotting import AnimatedOrbitPlotter
 
 
 def test_type_of_anim():
-    op = StaticOrbitPlotter()
+    op = AnimatedOrbitPlotter()
     ss = iss
     k = op.anim(ss)
     assert type(k) == matplotlib.animation.FuncAnimation
