17e042c8170fa6008afb8a9c51757422d329da94
Yash-10 <yashgondhalekar567@gmail.com>
2021-06-17 17:53:20 +0530

    Add tests for altitude crossing
    Reformat code
    
    Remove print
    
    Conver R to value from quantity
    
    Change test fucntion name
    
    Make an Event base class and inherit others
    
    Modify tests to handle class structure change
    
    Modify tests to handle class structure change
    
    Reformat code
    
    Add relevant changes to doc lithobrake example
    
    Make variable name and function signature changes
    
    Modify tests to include signature change
    
    Reformat code
    
    Add direction equal to -1 for altitude cross event
    
    Revert doc example changes
    
    Make method arguments without units
    
    Add .value for extracting value
    
    Reformat code
    
    Remove unecessary comment

--- src/poliastro/twobody/events.py
+++ src/poliastro/twobody/events.py
@@ -2,67 +2,81 @@ from astropy import units as u
 from numpy.linalg import norm
 
 
-class LithobrakeEvent:
+class Event:
+    """Base class for event functionalities.
+
+    Parameters
+    ----------
+    terminal: bool
+        Whether to terminate integration if this event occurs.
+    direction: float
+        Handle triggering of event.
+
+    """
+
+    def __init__(self, terminal, direction):
+        self._terminal, self._direction = terminal, direction
+        self._last_t = None
+
+    @property
+    def terminal(self):
+        return self._terminal
+
+    @property
+    def direction(self):
+        return self._direction
+
+    @property
+    def last_t(self):
+        return self._last_t * u.s
+
+    def __call__(self, t, u, k):
+        raise NotImplementedError
+
+
+class AltitudeCrossEvent(Event):
+    """Detect if a satellite crosses a specific threshold altitude.
+
+    Parameters
+    ----------
+    alt: float
+        Threshold altitude (km).
+    R: float
+        Radius of the attractor (km).
+    terminal: bool
+        Whether to terminate integration if this event occurs.
+    direction: float
+        Handle triggering of event based on whether altitude is crossed from above
+        or below, defaults to -1, i.e., event is triggered only if altitude is
+        crossed from above (decreasing altitude).
+
+    """
+
+    def __init__(self, alt, R, terminal=True, direction=-1):
+        super().__init__(terminal, direction)
+        self._R = R
+        self._alt = alt  # Threshold altitude from the ground.
+
+    def __call__(self, t, u, k):
+        self._last_t = t
+        r_norm = norm(u[:3])
+
+        return (
+            r_norm - self._R - self._alt
+        )  # If this goes from +ve to -ve, altitude is decreasing.
+
+
+class LithobrakeEvent(AltitudeCrossEvent):
     """Terminal event that detects impact with the attractor surface.
 
     Parameters
     ----------
     R : float
-        Radius of the attractor.
-
-    """
-
-    def __init__(self, R):
-        self._R = R
-        self._last_t = None
-
-    @property
-    def terminal(self):
-        # Tell SciPy to stop the integration at H = R (impact)
-        return True
-
-    @property
-    def last_t(self):
-        return self._last_t * u.s
-
-    def __call__(self, t, u, k):
-        self._last_t = t
-        H = norm(u[:3])
-        # SciPy will search for H - R = 0
-        print(H - self._R)
-        return H - self._R
-
-
-class AltitudeCrossEvent:
-    """Detect if a satellite crosses a specific threshold altitude.
-
-    Parameters
-    ----------
-    R: ~astropy.units.Quantity
         Radius of the attractor (km).
-    thresh_H: ~astropy.units.Quantity
-        Threshold altitude (in km), defaults to 100 km.
     terminal: bool
-        Whether to terminate integration if this event occurs, defaults to True.
+        Whether to terminate integration if this event occurs.
 
     """
-    def __init__(self, R, thresh_H=100*u.km, terminal=True):
-        self._R = R.to(u.km).value
-        self._thresh_H = thresh_H.to(u.km).value  # Threshold height from the ground.
-        self._terminal = terminal
-        self._last_t = None
 
-    @property
-    def terminal(self):
-        # Orekit's API stops propagation when descending, but not when ascending.
-        return self._terminal
-
-    @property
-    def last_t(self):
-        return self._last_t * u.s
-
-    def __call__(self, t, u, k):
-        self._last_t = t
-        H = norm(u[:3])
-        # H is from the center of the attractor.
-        return H - self._R - self._thresh_H  # If this goes from +ve to -ve, altitude is decreasing.
+    def __init__(self, R, terminal=True):
+        super().__init__(0, R, terminal, direction=-1)
