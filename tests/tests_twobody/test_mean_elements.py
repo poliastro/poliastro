722caf9b4b0d9507f49d8b52163617634dbe0010
Yash-10 <yashgondhalekar567@gmail.com>
2021-06-26 18:07:44 +0530

    Add test for mean_elements

---
+++ tests/tests_twobody/test_mean_elements.py
@@ -0,0 +1,9 @@
+import pytest
+from poliastro.twobody.mean_elements import get_mean_elements
+
+def test_get_mean_elements_raises_error_if_invalid_body():
+    body = "Sun"
+
+    with pytest.raises(ValueError) as excinfo:
+        get_mean_elements(body)
+    assert f"The input body is invalid." in excinfo.exconly()
