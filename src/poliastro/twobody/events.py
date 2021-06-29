eb9a9727b18957e5b8b184e55085882a55dce6e8
Yash-10 <yashgondhalekar567@gmail.com>
2021-06-29 23:24:50 +0530

    Add lat and lon detector

--- src/poliastro/twobody/events.py
+++ src/poliastro/twobody/events.py
@@ -1,6 +1,13 @@
 from astropy import units as u
 from numpy.linalg import norm
 
+from astropy.time import Time
+from astropy.coordinates import (
+    GCRS,
+    ITRS,
+    CartesianRepresentation,
+    SphericalRepresentation,
+)
 
 class Event:
     """Base class for event functionalities.
@@ -80,3 +87,74 @@ class LithobrakeEvent(AltitudeCrossEvent):
 
     def __init__(self, R, terminal=True):
         super().__init__(0, R, terminal, direction=-1)
+
+
+class LatitudeCrossEvent(Event):
+    """Detect if a satellite crosses a specific threshold latitude.
+
+    Parameters
+    ----------
+    lat: float
+        Threshold latitude (km).
+    R: float
+        Radius of the attractor (km).
+    terminal: bool
+        Whether to terminate integration if this event occurs, defaults to True.
+    direction: float
+        Handle triggering of event based on whether latitude is crossed from above
+        or below, defaults to 0, i.e., event is triggered while traversing from both directions.
+
+    """
+    def __init__(self, lat, R, terminal=True, direction=0):
+        super().__init__(terminal, direction)
+        self._R = R
+        self._lat = lat  # Threshold latitude (in degrees).
+
+    def __call__(self, t, u, k):
+        self._last_t = t
+        xyz = u[:3]
+
+        obstime = Time(self._last_t, format="jd")
+        gcrs_xyz = GCRS(
+            xyz, obstime=obstime, representation_type=CartesianRepresentation,
+        )
+        itrs_xyz = gcrs_xyz.transform_to(ITRS(obstime=obstime))
+        itrs_latlon_pos = itrs_xyz.represent_as(SphericalRepresentation)
+        orbit_lat = itrs_latlon_pos.lat.value
+        return self._lat - orbit_lat
+
+
+class LongitudeCrossEvent(Event):
+    """Detect if a satellite crosses a specific threshold latitude.
+
+    Parameters
+    ----------
+    lon: float
+        Threshold longitude (km).
+    R: float
+        Radius of the attractor (km).
+    terminal: bool
+        Whether to terminate integration if this event occurs, defaults to True.
+    direction: float
+        Handle triggering of event based on whether longitude is crossed from above
+        or below, defaults to 0, i.e., event is triggered while traversing from both directions.
+
+    """
+    def __init__(self, lon, R, terminal=True, direction=0):
+        super().__init__(terminal, direction)
+        self._R = R
+        self._lon = lon  # Threshold longitude (in degrees).
+
+    def __call__(self, t, u, k):
+        self._last_t = t
+        xyz = u[:3]
+
+        obstime = Time(self._last_t, format="jd")
+        gcrs_xyz = GCRS(
+            xyz, obstime=obstime, representation_type=CartesianRepresentation,
+        )
+        itrs_xyz = gcrs_xyz.transform_to(ITRS(obstime=obstime))
+        itrs_latlon_pos = itrs_xyz.represent_as(SphericalRepresentation)
+        orbit_lon = itrs_latlon_pos.lon.value
+
+        return self._lon - orbit_lon
