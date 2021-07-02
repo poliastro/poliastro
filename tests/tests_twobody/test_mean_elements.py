2395d08c672250b5df273eb36415c8200dd7f801
Yash-10 <yashgondhalekar567@gmail.com>
2021-07-02 11:59:57 +0530

    Add test for error check

--- tests/tests_twobody/test_mean_elements.py
+++ tests/tests_twobody/test_mean_elements.py
@@ -1,10 +1,11 @@
 import pytest
 
+from poliastro.bodies import Sun
 from poliastro.twobody.mean_elements import get_mean_elements
 
 
 def test_get_mean_elements_raises_error_if_invalid_body():
-    body = "PlanetNine"
+    body = Sun
 
     with pytest.raises(ValueError) as excinfo:
         get_mean_elements(body)
