43e47070c82bbadfc28c61c278d6d2b9e41dfff6
Jorge M.G <jorge@on-orbit.dev>
2021-10-08 09:28:30 +0200

    Unify atmospheric models input parameters

--- src/poliastro/earth/atmosphere/jacchia.py
+++ src/poliastro/earth/atmosphere/jacchia.py
@@ -14,15 +14,16 @@ k = 1.380622e-23 * u.J / u.K
 class Jacchia77:
     """Holds the model for U.S Standard Atmosphere 1962."""
 
-    def __init__(self):
+    def __init__(self, Texo):
         self.E5M = np.zeros(11)
         self.E6P = np.zeros(11)
         self.x = 0.0
         self.y = 0.0
+        self.Texo = Texo
 
-    def _altitude_profile(self, alt, Texo):
+    def _altitude_profile(self, alt):
         Z, T, CN2, CO2, CO, CAr, CHe, CH, CM, WM = _altitude_profile_fast(
-            alt, Texo, self.x, self.y, self.E5M, self.E6P
+            alt, self.Texo.to(u.K).value, self.x, self.y, self.E5M, self.E6P
         )
         (
             self.Z,
@@ -60,23 +61,21 @@ class Jacchia77:
             self.WM,
         )
 
-    def _H_correction(self, alt, Texo):
+    def _H_correction(self, alt):
         """Calculate [H] from Jacchia 1977 formulas"""
-        _H_correction_fast(alt, Texo)
+        _H_correction_fast(alt, self.Texo)
 
-    def _O_and_O2_correction(self, alt, Texo):
+    def _O_and_O2_correction(self, alt):
         """Add Jacchia 1977 empirical corrections to [O] and [O2]"""
-        _O_and_O2_correction_fast(alt, Texo)
+        _O_and_O2_correction_fast(alt, self.Texo)
 
-    def altitude_profile(self, alt, Texo):
+    def altitude_profile(self, alt):
         """Solves for atmospheric altitude profile at given altitude and exospheric temperature.
 
         Parameters
         ----------
         alt: ~astropy.units.Quantity
             Geometric/Geopotential altitude.
-        Texo: ~astropy.units.Quantity
-            Exospheric temperature
 
         Returns
         -------
@@ -86,31 +85,29 @@ class Jacchia77:
         # checking if the units entered are km
         if alt.unit == u.km:
             if 150 <= alt.value < 500:
-                alt_properties = self._altitude_profile(500, Texo.value)
+                alt_properties = self._altitude_profile(500)
             else:
-                alt_properties = self._altitude_profile(alt.value, Texo.value)
+                alt_properties = self._altitude_profile(alt.value)
 
         return [last[int(alt.value)] for last in alt_properties]
 
-    def temperature(self, alt, Texo):
+    def temperature(self, alt):
         """Solves for temperature at given altitude and exospheric temperature.
 
         Parameters
         ----------
         alt: ~astropy.units.Quantity
             Geometric/Geopotential altitude.
-        Texo: ~astropy.units.Quantity
-            Exospheric temperature
 
         Returns
         -------
         T: ~astropy.units.Quantity
             Absolute temeperature  and exospheric temperature
         """
-        T = self.altitude_profile(alt, Texo)[1]
+        T = self.altitude_profile(alt)[1]
         return T
 
-    def pressure(self, alt, Texo):
+    def pressure(self, alt):
         """Solves pressure at given altitude and exospheric temperature.
 
         Parameters
@@ -125,30 +122,28 @@ class Jacchia77:
         p: ~astropy.units.Quantity
             Pressure at given altitude  and exospheric temperature.
         """
-        alt_profile = self.altitude_profile(alt, Texo)
+        alt_profile = self.altitude_profile(alt)
         T, number_density = alt_profile[1], alt_profile[8]
 
         # using eqn(42) of COESA76
         pressure = number_density * k * T
         return pressure
 
-    def density(self, alt, Texo):
+    def density(self, alt):
         """Solves density at given altitude and exospheric temperature.
 
         Parameters
         ----------
         alt: ~astropy.units.Quantity
             Geometric/Geopotential altitude.
-        Texo: ~astropy.units.Quantity
-            Exospheric Temperature
 
         Returns
         -------
         rho: ~astropy.units.Quantity
             Density at given altitude and exospheric temperature.
         """
-        alt_profile = self.altitude_profile(alt, Texo)
-        P = self.pressure(alt, Texo)
+        alt_profile = self.altitude_profile(alt)
+        P = self.pressure(alt)
 
         # using eqn(42) of COESA76
         rho = P * alt_profile[9] / (R * alt_profile[1])
