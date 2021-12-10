9b905e0bc601de2951760171c12e2f0a00b0e7ac
Juan Luis Cano Rodríguez <hello@juanlu.space>
2021-12-10 03:12:20 +0100

    Adjust tests

--- tests/test_sensors.py
+++ tests/test_sensors.py
@@ -14,15 +14,15 @@ from poliastro.sensors import ground_range_diff_at_azimuth, min_and_max_ground_r
             800 * u.km,
             (25 * u.deg).to(u.rad),
             (40 * u.deg).to(u.rad),
-            0.18736414 * u.rad,
-            0.06649331 * u.rad,
+            10.73517 * u.deg,
+            3.80977 * u.deg,
         ),
         (
             800 * u.km,
             (25 * u.deg).to(u.rad),
             (0 * u.deg).to(u.rad),
-            0.0278967 * u.rad,
-            -0.0278967 * u.rad,
+            1.5984 * u.deg,
+            -1.5984 * u.deg,
         ),
     ],
 )
@@ -32,12 +32,11 @@ def test_max_and_min_ground_range(
 
     R = Earth.R.to(u.km)
     lat_lon_min, lat_lon_max = min_and_max_ground_range(altitude, fov, boresight, R)
-    assert_quantity_allclose(lat_lon_max, expected_lat_lon_max)
-    assert_quantity_allclose(lat_lon_min, expected_lat_lon_min)
+    assert_quantity_allclose(lat_lon_max, expected_lat_lon_max, rtol=1e-4)
+    assert_quantity_allclose(lat_lon_min, expected_lat_lon_min, rtol=1e-4)
 
 
 # Example taken from "Fundamentals of Astrodynamics and Applications", 4th ed (2013)" by David A. Vallado, pages 859-860
-@pytest.mark.xfail(reason="Wrong result, must check")
 @pytest.mark.parametrize(
     "altitude, fov, boresight, azimuth, nadir_lat, nadir_lon, expected_ground_range_diff, expected_target_lat, expected_target_lon",
     [
@@ -48,9 +47,9 @@ def test_max_and_min_ground_range(
             (140 * u.deg).to(u.rad),
             (50 * u.deg).to(u.rad),
             (40 * u.deg).to(u.rad),
-            0.06197359 * u.rad,
-            0.82639242 * u.rad,
-            0.75442901 * u.rad,
+            (6.9254 / 2) * u.deg,
+            44.9926 * u.deg,
+            45.7577 * u.deg,
         ),
     ],
 )
@@ -70,9 +69,9 @@ def test_ground_range_diff_at_azimuth(
     ground_range_diff, target_lat, target_lon = ground_range_diff_at_azimuth(
         altitude, fov, boresight, azimuth, nadir_lat, nadir_lon, R
     )
-    assert_quantity_allclose(ground_range_diff, expected_ground_range_diff)
-    assert_quantity_allclose(target_lat, expected_target_lat)
-    assert_quantity_allclose(target_lon, expected_target_lon)
+    assert_quantity_allclose(ground_range_diff, expected_ground_range_diff, rtol=1e-5)
+    assert_quantity_allclose(target_lat, expected_target_lat, rtol=1e-6)
+    assert_quantity_allclose(target_lon, expected_target_lon, rtol=1e-6)
 
 
 # Example taken from "Fundamentals of Astrodynamics and Applications", 4th ed (2013)" by David A. Vallado, pages 859-860
