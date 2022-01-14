554e2c181c412eeafc9b2d259bfdad9a71d9e748
Kevin Charls <56789584+JackCrusoe47@users.noreply.github.com>
2022-01-14 21:33:07 +0530

    Correction recseries
    Correction

--- src/poliastro/core/propagation/__init__.py
+++ src/poliastro/core/propagation/__init__.py
@@ -9,7 +9,7 @@ from poliastro.core.propagation.gooding import gooding, gooding_coe
 from poliastro.core.propagation.markley import markley, markley_coe
 from poliastro.core.propagation.mikkola import mikkola, mikkola_coe
 from poliastro.core.propagation.pimienta import pimienta, pimienta_coe
-from poliastro.core.propagation.recSeries import recseries, recseries_coe
+from poliastro.core.propagation.recseries import recseries, recseries_coe
 from poliastro.core.propagation.vallado import vallado
 
 __all__ = [
