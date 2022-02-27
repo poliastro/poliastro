d61e82d2e6aa867181255c524fcf4947769fe552
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-02-27 21:18:20 +0100

    turn states into module folder

---
+++ src/poliastro/twobody/states/__init__.py
@@ -0,0 +1,14 @@
+
+from poliastro.twobody.states.scalar import (
+    BaseState,
+    ClassicalState,
+    RVState,
+    ModifiedEquinoctialState,
+)
+
+__all__ = [
+    "BaseState",
+    "ClassicalState",
+    "RVState",
+    "ModifiedEquinoctialState",
+]
