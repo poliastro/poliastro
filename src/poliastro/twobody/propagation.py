13b7647aa9b6375bef55a346591b68ed657f1e81
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-03-01 01:15:38 +0100

    parallel test fix: ordered propagators

--- src/poliastro/twobody/propagation.py
+++ src/poliastro/twobody/propagation.py
@@ -550,6 +550,7 @@ HYPERBOLIC_PROPAGATORS = [
     danby,
     cowell,
 ]
-ALL_PROPAGATORS = list(
-    set(ELLIPTIC_PROPAGATORS + PARABOLIC_PROPAGATORS + HYPERBOLIC_PROPAGATORS)
+ALL_PROPAGATORS = sorted(
+    set(ELLIPTIC_PROPAGATORS + PARABOLIC_PROPAGATORS + HYPERBOLIC_PROPAGATORS),
+    key = lambda func: func.__name__,  # enable parallel testing with guaranteed order of propagators
 )
