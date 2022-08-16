41ec8535f814c976616e506a4d4229d656e36423
DhruvJ9922 <dhruvj9922@gmail.com>
2022-08-16 17:30:08 -0400

    Updated to use bodies.Body

--- src/poliastro/bodies.py
+++ src/poliastro/bodies.py
@@ -14,6 +14,10 @@ Contains some predefined bodies of the Solar System:
 * Neptune (♆)
 * Pluto (♇)
 
+Phobos
+Deimos
+
+
 and a way to define new bodies (:py:class:`~Body` class).
 
 Data references can be found in :py:mod:`~poliastro.constants`
@@ -52,6 +56,7 @@ class Body(
             "J2",
             "J3",
             "mass",
+            "mean_a",
         ],
     )
 ):
@@ -70,6 +75,7 @@ class Body(
         J2=0.0 * u.one,
         J3=0.0 * u.one,
         mass=None,
+        mean_a = 0.0 * u.km,
     ):
         if mass is None:
             mass = k / G
@@ -87,6 +93,7 @@ class Body(
             _q(J2),
             _q(J3),
             _q(mass),
+            _q(mean_a),
         )
 
     @property
@@ -187,6 +194,7 @@ Mercury = SolarSystemPlanet(
     R_mean=constants.R_mean_mercury,
     R_polar=constants.R_polar_mercury,
     rotational_period=constants.rotational_period_mercury,
+    mean_a=constants.mean_a_mercury,
 )
 
 
@@ -201,6 +209,7 @@ Venus = SolarSystemPlanet(
     rotational_period=constants.rotational_period_venus,
     J2=constants.J2_venus,
     J3=constants.J3_venus,
+    mean_a=constants.mean_a_venus,
 )
 
 
@@ -216,6 +225,7 @@ Earth = SolarSystemPlanet(
     mass=constants.M_earth,
     J2=constants.J2_earth,
     J3=constants.J3_earth,
+    mean_a=constants.mean_a_earth,
 )
 
 
@@ -230,6 +240,7 @@ Mars = SolarSystemPlanet(
     rotational_period=constants.rotational_period_mars,
     J2=constants.J2_mars,
     J3=constants.J3_mars,
+    mean_a=constants.mean_a_mars,
 )
 
 
@@ -243,6 +254,7 @@ Jupiter = SolarSystemPlanet(
     R_polar=constants.R_polar_jupiter,
     rotational_period=constants.rotational_period_jupiter,
     mass=constants.M_jupiter,
+    mean_a=constants.mean_a_jupiter,
 )
 
 
@@ -255,6 +267,7 @@ Saturn = SolarSystemPlanet(
     R_mean=constants.R_mean_saturn,
     R_polar=constants.R_polar_saturn,
     rotational_period=constants.rotational_period_saturn,
+    mean_a=constants.mean_a_saturn,
 )
 
 
@@ -267,6 +280,7 @@ Uranus = SolarSystemPlanet(
     R_mean=constants.R_mean_uranus,
     R_polar=constants.R_polar_uranus,
     rotational_period=constants.rotational_period_uranus,
+    mean_a=constants.mean_a_uranus,
 )
 
 
@@ -279,6 +293,7 @@ Neptune = SolarSystemPlanet(
     R_mean=constants.R_mean_neptune,
     R_polar=constants.R_polar_neptune,
     rotational_period=constants.rotational_period_neptune,
+    mean_a=constants.mean_a_neptune,
 )
 
 
@@ -291,7 +306,7 @@ Pluto = Body(
     R_mean=constants.R_mean_pluto,
     R_polar=constants.R_polar_pluto,
     rotational_period=constants.rotational_period_pluto,
-)
+) # No mean_a_pluto as Pluto is officially not a planet around Sun
 
 
 Moon = Body(
@@ -303,4 +318,75 @@ Moon = Body(
     R_mean=constants.R_mean_moon,
     R_polar=constants.R_polar_moon,
     rotational_period=constants.rotational_period_moon,
+    mean_a=constants.mean_a_moon
 )
+
+
+Phobos = Body(
+    parent=Mars,
+    k=constants.GM_phobos,
+    name="Phobos",
+    mean_a=constants.mean_a_phobos,
+)
+
+Deimoms = Body(
+    parent=Mars,
+    k=constants.GM_deimos,
+    name="Deimos",
+    mean_a=constants.mean_a_deimos,
+)
+
+Europa = Body(
+    parent=Jupiter,
+    k=constants.GM_europa,
+    name="Europa",
+    mean_a=constants.mean_a_europa,
+)
+
+Ganymede = Body(
+    parent=Jupiter,
+    k=constants.GM_ganymede,
+    name="Ganymede",
+    mean_a=constants.mean_a_ganymede,
+)
+
+Enceladus = Body(
+    parent=Saturn,
+    k=constants.GM_enceladus,
+    name="Enceladus",
+    mean_a=constants.mean_a_enceladus,
+)
+
+Titan = Body(
+    parent=Saturn,
+    k=constants.GM_titan,
+    name="Titan",
+    mean_a=constants.mean_a_titan,
+)
+
+Titania = Body(
+    parent=Uranus,
+    k=constants.GM_titania,
+    name="Titania",
+    mean_a=constants.mean_a_titania,
+)
+
+Triton = Body(
+    parent=Neptune,
+    k=constants.GM_triton,
+    name="Triton",
+    mean_a=constants.mean_a_triton,
+)   
+
+Charon = Body(
+    parent=Pluto,
+    k=constants.GM_charon,
+    name="charon",
+    mean_a=constants.mean_a_charon,
+)
+
+
+
+
+
+
---
+++ src/poliastro/core/threebody/cr3bp_quantities_calculations.py
@@ -0,0 +1,43 @@
+"""
+@author: Dhruv Jain, Multi-Body Dynamics Research Group, Purdue University
+"""
+
+from astropy import units as u
+
+def calculate_mu(mu1, mu2):
+    """Calculate mu of CR3BP
+    Parameters
+    ----------
+    mu1: float, km^3*s^-2
+        mu of P1
+    mu1: float, km^3*s^-2
+        mu of P2
+
+    Returns
+    -------
+    mu: float, dimensionless
+        mu2/(mu1+mu2), mu2<mu1
+    """
+    return (mu2 / (mu1 + mu2)) * u.one
+
+def calculate_tstar(mu1, mu2, lstar):
+    """Calculate t* of CR3BP
+    Parameters
+    ----------
+    mu1: float, dimensionless
+        mu of P1
+    mu1: float, dimensionless
+        mu of P2
+    lstar: float, km
+        Characterisitc length of P1 - P2 system
+
+    Returns
+    -------
+    t*: float, sec
+        Characterisitc time of P1-P2 system
+
+    .. math::
+
+        \sqrt{\frac{l*^3}{M1+M2}}
+    """
+    return (lstar**3 / (mu1 + mu2)) ** 0.5
