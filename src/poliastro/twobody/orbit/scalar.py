Fix unit of h_vec in cross product calculation
Calculating h_vec using self.v.to(u.km / u.s) in np.cross(r, v)
retains u.km / u.s as its unit. Therefore, multiplying np.cross(r, v)
with ((u.km)^2 / u.s) sets the unit of h_vec to ((u.km)^3 / (u.s)^2).

Instead self.v.to_value(u.km / u.s) should be used to keep np.cross(r, v)
unitless.

src/poliastro/twobody/orbit/scalar.py
@@ -173,7 +173,7 @@ class Orbit(OrbitCreationMixin):
     def h_vec(self):
         """Specific angular momentum vector."""
         h_vec = (
-            np.cross(self.r.to_value(u.km), self.v.to(u.km / u.s))
+            np.cross(self.r.to_value(u.km), self.v.to_value(u.km / u.s))
             * u.km**2
             / u.s
         )