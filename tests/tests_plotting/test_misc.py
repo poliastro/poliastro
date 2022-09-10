a1b2de2f4ca72ea6ed55a1e9cdba51ff3b725596
Jorge Martínez Garrido <jorge@astrodevelopers.com>
2022-09-10 12:51:03 +0200

    FIX: tests on miscellanea

--- tests/tests_plotting/test_misc.py
+++ tests/tests_plotting/test_misc.py
@@ -1,39 +1,42 @@
 import pytest
 from matplotlib import pyplot as plt
 
-from poliastro.plotting import OrbitPlotter2D, OrbitPlotter3D
+from poliastro.plotting import OrbitPlotter
+from poliastro.plotting.orbit.backends import (
+    SUPPORTED_ORBIT_PLOTTER_BACKENDS,
+)
 from poliastro.plotting.misc import plot_solar_system
 
 
 @pytest.mark.parametrize("outer, expected", [(True, 8), (False, 4)])
 def test_plot_solar_system_has_expected_number_of_orbits(outer, expected):
-    assert len(plot_solar_system(outer).trajectories) == expected
+    assert len(plot_solar_system(outer=outer).trajectories) == expected
 
 
-@pytest.mark.parametrize(
-    "use_3d, plotter_class", [(True, OrbitPlotter3D), (False, OrbitPlotter2D)]
-)
-def test_plot_solar_system_uses_expected_orbitplotter(use_3d, plotter_class):
+@pytest.mark.parametrize("backend_name", SUPPORTED_ORBIT_PLOTTER_BACKENDS)
+def test_plot_solar_system_uses_expected_orbitplotter(backend_name):
+    print(backend_name)
     assert isinstance(
-        plot_solar_system(use_3d=use_3d, interactive=True), plotter_class
+        plot_solar_system(backend_name=backend_name).backend,
+        SUPPORTED_ORBIT_PLOTTER_BACKENDS[backend_name]
     )
 
 
-def test_plot_solar_system_raises_error_if_use_3d_and_non_interactive():
-    with pytest.raises(ValueError) as excinfo:
-        plot_solar_system(use_3d=True)
-    assert "The static plotter does not support 3D" in excinfo.exconly()
-
-
 @pytest.mark.mpl_image_compare
-def test_plot_inner_solar_system_static(earth_perihelion):
-    plot_solar_system(outer=False, epoch=earth_perihelion)
-
+def test_plot_inner_solar_system_usgin_matplotlib2D_backend(earth_perihelion):
+    plot_solar_system(
+        backend_name="matplotlib2D",
+        epoch=earth_perihelion,
+        outer=False,
+    )
     return plt.gcf()
 
 
 @pytest.mark.mpl_image_compare
-def test_plot_outer_solar_system_static(earth_perihelion):
-    plot_solar_system(outer=True, epoch=earth_perihelion)
-
+def test_plot_outer_solar_system_using_matplotlib2D_backend(earth_perihelion):
+    plot_solar_system(
+        backend_name="matplotlib2D",
+        epoch=earth_perihelion,
+        outer=True,
+    )
     return plt.gcf()
