914edbdfd46081dfb17d3728f06ab723e0894fbf
Jorge Martínez Garrido <jorge@astrodevelopers.com>
2022-10-22 13:37:54 +0200

    FIX: dict merge syntax

--- src/poliastro/plotting/orbit/backends/__init__.py
+++ src/poliastro/plotting/orbit/backends/__init__.py
@@ -14,9 +14,10 @@ SUPPORTED_ORBIT_PLOTTER_BACKENDS_3D = {}
 """A dictionary relating 3D orbit plotter backends and their classes."""
 
 
-SUPPORTED_ORBIT_PLOTTER_BACKENDS = (
-    SUPPORTED_ORBIT_PLOTTER_BACKENDS_2D | SUPPORTED_ORBIT_PLOTTER_BACKENDS_3D
-)
+SUPPORTED_ORBIT_PLOTTER_BACKENDS = {
+    **SUPPORTED_ORBIT_PLOTTER_BACKENDS_2D,
+    **SUPPORTED_ORBIT_PLOTTER_BACKENDS_3D,
+}
 """A dictionary relating orbit plotter backends and their classes."""
 
 
